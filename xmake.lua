f73150b097eadc406791b27bcc7111c0c43f2df5
qylWheels <command1748165360@126.com>
2026-06-26 19:27:42 +0800

    Add fltk package requirement for xavgui and main project

--- xmake.lua
+++ xmake.lua
@@ -2,7 +2,7 @@ add_rules("mode.debug", "mode.release")
 
 set_languages("c++20")
 
-add_requires("leveldb", "cryptopp", "protobuf-cpp")
+add_requires("leveldb", "cryptopp", "protobuf-cpp", "fltk")
 
 includes("xavgui")
 includes("xavlib")
