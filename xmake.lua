3923da9b23fc5ec5ab2205502df032fbf407ab60
qylWheels <command1748165360@126.com>
2026-06-26 19:38:35 +0800

    Replace fltk package requirement with wxwidgets in xmake.lua.

--- xmake.lua
+++ xmake.lua
@@ -2,7 +2,7 @@ add_rules("mode.debug", "mode.release")
 
 set_languages("c++20")
 
-add_requires("leveldb", "cryptopp", "protobuf-cpp", "fltk")
+add_requires("leveldb", "cryptopp", "protobuf-cpp", "wxwidgets")
 
 includes("xavgui")
 includes("xavlib")
