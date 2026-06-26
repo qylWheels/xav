c6247bdfdb523782ff2ff4fe5787dcf089fa4bcb
qylWheels <command1748165360@126.com>
2026-06-26 13:20:49 +0800

    Add Scanner class declaration.

---
+++ xavlib/scanner.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <filesystem>
+#include <string>
+
+class Scanner {
+public:
+    Scanner();
+    ~Scanner();
+    Scanner(const Scanner&) = delete;
+    Scanner& operator=(const Scanner&) = delete;
+    Scanner(Scanner&&) = delete;
+    Scanner& operator=(Scanner&&) = delete;
+
+public:
+    void scan(const char* path, int nthreads);
+    void scan(const std::string& path, int nthreads);
+    void scan(const std::filesystem::path& path, int nthreads);
+};
