9e50cd4233ee1a009fedd7f618db77b41109c1ea
qylWheels <command1748165360@126.com>
2026-07-01 00:43:27 +0800

    Import element-plus/icons-vue.

---
+++ xavadmin/src/components/SingleMachineDashboard.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="layout">
+        <el-container>
+            <el-header>
+                <h2>Xav Admin Management Console</h2>
+            </el-header>
+            <el-main>
+                <el-card>
+                    <template #header>
+                        <span>Basic Information of {{ machineName }}</span>
+                    </template>
+                    <template #default>
+                        <i class="fa-solid fa-thumbs-up"></i>
+                    </template>
+                    <template #footer>
+
+                    </template>
+                </el-card>
+            </el-main>
+        </el-container>
+    </div>
+</template>
+
+<style scoped></style>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const machineName = ref("Comma")
+</script>
