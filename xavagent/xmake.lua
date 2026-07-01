2c69d835bf58b46fd15a20d9f5446c30bd37954e
qylWheels <command1748165360@126.com>
2026-07-01 20:28:19 +0800

    Add dependency websocketpp.

--- xavagent/xmake.lua
+++ xavagent/xmake.lua
@@ -2,7 +2,9 @@ target("xavagent")
     set_kind("binary")
     add_files("**.cpp")
     add_deps("xavlib")
-    add_packages("leveldb", "cryptopp", "wxwidgets", "oatpp")
+    add_packages(
+        "leveldb", "cryptopp", "wxwidgets", "oatpp", "websocketpp"
+    )
 
     on_run(function (target)
         import("core.base.option")
