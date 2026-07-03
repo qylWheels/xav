fb0b64371aedf039b03d0f6ce92090708d4dded3
qylWheels <command1748165360@126.com>
2026-07-03 18:18:35 +0800

    Restore Scanner and related restful api in XavAgentController.

--- xavagent/controller/xavagent_controller.h
+++ xavagent/controller/xavagent_controller.h
@@ -33,52 +33,50 @@ public:
         return createDtoResponse(Status::CODE_200, dto);
     }
 
-    // ENDPOINT("GET", "/scan/quick/start", quick_scan) {
-    //     if (this->scanner_.scan_status() != ScanStatus::Stopped) {
-    //         OATPP_LOGi("Xav Agent", "Quick scan is already running");
-    //         return this->createResponse(Status::CODE_403);
-    //     }
+    ENDPOINT("GET", "/scan/quick/start", quick_scan) {
+        if (this->scanner_.scan_status() != ScanStatus::Stopped) {
+            OATPP_LOGi("Xav Agent", "Quick scan is already running");
+            return this->createResponse(Status::CODE_403);
+        }
 
-    //     OATPP_LOGi("Xav Agent", "Execute quick scan");
+        OATPP_LOGi("Xav Agent", "Execute quick scan");
 
-    //     std::thread t([this]() {
-    //         // std::vector<std::string> critical_paths{
-    //         //     "/home", "/tmp",     "/var/tmp", "/bin",
-    //         //     "/sbin", "/usr/bin", "/usr/sbin"};
-    //         // FIXME: Only for test.
-    //         std::vector<const char*>
-    //         critical_paths{"/home/comma/projs/xav/"}; for (const auto& path :
-    //         critical_paths) {
-    //             this->scanner_.scan(path, 4);
-    //         }
-    //         OATPP_LOGi("Xav Agent", "Quick scan completed");
-    //     });
-    //     t.detach();
+        std::thread t([this]() {
+            // std::vector<std::string> critical_paths{
+            //     "/home", "/tmp",     "/var/tmp", "/bin",
+            //     "/sbin", "/usr/bin", "/usr/sbin"};
+            // FIXME: Only for test.
+            std::vector<const char*> critical_paths{"/home/comma/projs/xav/"};
+            for (const auto& path : critical_paths) {
+                this->scanner_.scan(path, 4);
+            }
+            OATPP_LOGi("Xav Agent", "Quick scan completed");
+        });
+        t.detach();
 
-    //     return this->createResponse(Status::CODE_200);
-    // }
+        return this->createResponse(Status::CODE_200);
+    }
 
-    // ENDPOINT("GET", "/scan/quick/status", quick_scan_status) {
-    //     auto status = ScanStatusDto::createShared();
-    //     status->scan_status =
-    //     ScanStatusEnumDto(this->scanner_.scan_status());
-    //     status->total_file_count = this->scanner_.total_file_count();
-    //     status->scanned_file_count = this->scanner_.scanned_file_count();
-    //     auto malware_infos = this->scanner_.malware_infos();
-    //     status->malware_infos = {};
-    //     for (const auto& info : malware_infos) {
-    //         auto info_dto = MalwareInfoDto::createShared();
-    //         info_dto->file_path = info.file_path.string();
-    //         info_dto->malware_name = std::format(
-    //             "{}.{}.{}",
-    //             malware_info::MalwareType_Name(info.malware_info.type()),
-    //             info.malware_info.family(), info.malware_info.variant());
-    //         status->malware_infos->push_back(info_dto);
-    //     }
-    //     status->curr_scanning_file =
-    //         this->scanner_.curr_scanning_file().string();
-    //     return this->createDtoResponse(Status::CODE_200, status);
-    // }
+    ENDPOINT("GET", "/scan/quick/status", quick_scan_status) {
+        auto status = ScanStatusDto::createShared();
+        status->scan_status = ScanStatusEnumDto(this->scanner_.scan_status());
+        status->total_file_count = this->scanner_.total_file_count();
+        status->scanned_file_count = this->scanner_.scanned_file_count();
+        auto malware_infos = this->scanner_.malware_infos();
+        status->malware_infos = {};
+        for (const auto& info : malware_infos) {
+            auto info_dto = MalwareInfoDto::createShared();
+            info_dto->file_path = info.file_path.string();
+            info_dto->malware_name = std::format(
+                "{}.{}.{}",
+                malware_info::MalwareType_Name(info.malware_info.type()),
+                info.malware_info.family(), info.malware_info.variant());
+            status->malware_infos->push_back(info_dto);
+        }
+        status->curr_scanning_file =
+            this->scanner_.curr_scanning_file().string();
+        return this->createDtoResponse(Status::CODE_200, status);
+    }
 
     ENDPOINT("GET", "/execution-monitor/status", execution_monitor_status) {
         auto status = ExecutionMonitorStatusDto::createShared();
@@ -90,7 +88,7 @@ public:
     }
 
 private:
-    // Scanner scanner_;
+    Scanner scanner_;
     ExecutionMonitor execution_monitor_;
 };
 }  // namespace xavagent
