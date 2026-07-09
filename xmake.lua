d4496708e9fe939bee630f6c6a07b375dea1d87a
qylWheels <command1748165360@126.com>
2026-07-10 00:30:54 +0800

    Remove rapidfuzz from required packages in xmake.lua.

--- xmake.lua
+++ xmake.lua
@@ -4,8 +4,7 @@ set_languages("c++20")
 
 add_requires(
     "leveldb", "cryptopp", "protobuf-cpp", "wxwidgets",
-    "oatpp", "spdlog", "yaml-cpp", "openssl", "outcome",
-    "rapidfuzz"
+    "oatpp", "spdlog", "yaml-cpp", "openssl", "outcome"
 )
 add_requires("apt::libyara-dev", {alias = "yara"})
 
