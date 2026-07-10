a86b3552fa154792674a6a95bd80b8e396d3dc41
qylWheels <command1748165360@126.com>
2026-07-11 00:27:54 +0800

    Refactor SingleMachineDashboard layout to include sidebar navigation.

--- xavadmin/src/components/SingleMachineDashboard.vue
+++ xavadmin/src/components/SingleMachineDashboard.vue
@@ -1,133 +1,172 @@
 <template>
     <div class="layout">
         <el-container>
-            <el-header>
-                <h2>Xav Admin Management Platform</h2>
-            </el-header>
-            <el-main>
-                <el-row>
-                    <el-col :span="24">
-                        <el-card>
-                            <template #header>
-                                <el-icon>
-                                    <Monitor />
-                                </el-icon>
-                                <span>&nbsp;&nbsp;Basic Information of {{ machineName }}</span>
-                            </template>
-                            <template #default>
-                                <el-row>
-                                    <el-col :span="2"></el-col>
-                                    <el-col :span="2">
-                                        <el-icon class="monitor-icon" size="128" :color="successColor">
-                                            <Monitor />
-                                        </el-icon>
-                                    </el-col>
-                                    <el-col :span="1"></el-col>
-                                    <el-col :span="5">
-                                        <el-row class="basic-info">
-                                            <el-text>System: {{ system }}</el-text>
-                                        </el-row>
-                                        <el-row class="basic-info">
-                                            <el-text>Connection Status: </el-text>
-                                            <el-tag type="success" class="basic-info-tag">Online</el-tag>
-                                        </el-row>
-                                        <el-row class="basic-info">
-                                            <el-text>Security Status: </el-text>
-                                            <el-tag type="success" class="basic-info-tag">Safe</el-tag>
-                                        </el-row>
-                                        <el-button type="danger" plain disabled>Disconnect</el-button>
-                                    </el-col>
-                                    <el-col :span="4">
-                                        <el-statistic title="On Access Scanner Scanned Objects"
-                                            :value="onAccessScannerScanObjectCount" />
-                                        <div class="statistic-margin" />
-                                        <el-statistic title="On Access Scanner Blocked Threats"
-                                            :value="onAccessScannerBlockedThreatsCount"
-                                            :value-style="{ color: dangerColor }" />
-                                    </el-col>
-                                    <el-col :span="3">
-                                        <el-statistic title="Total Events" :value="totalEventCount" />
-                                        <div class="statistic-margin" />
-                                        <el-statistic title="Suspicious Events" :value="suspiciousEventCount"
-                                            :value-style="{ color: warningColor }" />
-                                    </el-col>
-                                    <el-col :span="3">
-                                        <el-progress type="dashboard" :percentage="cpuUsage" :color="dangerColor">
-                                            <template #default="{ percentage }">
-                                                <el-text class="percentage-value">{{ percentage }}%</el-text>
-                                                <el-text class="percentage-label">CPU Usage</el-text>
-                                            </template>
-                                        </el-progress>
-                                    </el-col>
-                                    <el-col :span="3">
-                                        <el-progress type="dashboard" :percentage="memoryUsage" :color="warningColor">
-                                            <template #default="{ percentage }">
-                                                <el-text class="percentage-value">{{ percentage }}%</el-text>
-                                                <el-text class="percentage-label">Memory Usage</el-text>
-                                            </template>
-                                        </el-progress>
-                                    </el-col>
-                                </el-row>
-                            </template>
-                        </el-card>
-                    </el-col>
-                </el-row>
-                <el-row>
-                    <div class="card-margin"></div>
-                </el-row>
-                <el-row>
-                    <el-col :span="24">
-                        <el-card>
-                            <template #header>
-                                <el-icon>
-                                    <Search />
-                                </el-icon>
-                                <span>&nbsp;&nbsp;Scan</span>
-                            </template>
-                            <template #default>
-                                <el-row>
-                                    <el-button plain @click="handleQuickScanBtnClick"
-                                        :disabled="scanButtonDisabled">Quick
-                                        Scan</el-button>
-                                    <el-button plain disabled>Full Scan</el-button>
-                                    <el-button plain disabled>Custom Scan</el-button>
-                                </el-row>
-                                <div class="scan-margin"></div>
-                                <el-row>
-                                    <el-col :span="24">
-                                        <el-progress :percentage="scanProgress"
-                                            :status="scanProgressBarStatus"></el-progress>
-                                    </el-col>
-                                </el-row>
-                                <el-row>
-                                    <el-col :span="24">
-                                        <el-text>{{ scanStatusText }}</el-text>
-                                    </el-col>
-                                </el-row>
-                                <el-row>
-                                    <el-col :span="24">
-                                        <el-table :data="malwareInfos" :max-height="400">
-                                            <el-table-column prop="file_path" label="Path" />
-                                            <el-table-column prop="malware_name" label="Malware Name">
-                                                <template #default="scope">
-                                                    <el-text type="danger">{{ scope.row.malware_name }}</el-text>
+            <el-aside>
+                <el-menu default-active="1">
+                    <el-menu-item index="1">
+                        <el-icon>
+                            <Monitor />
+                        </el-icon>
+                        <template #title>
+                            Dashboard
+                        </template>
+                    </el-menu-item>
+                    <el-menu-item index="2">
+                        <el-icon>
+                            <Search />
+                        </el-icon>
+                        <template #title>
+                            Scan
+                        </template>
+                    </el-menu-item>
+                    <el-menu-item index="3">
+                        <el-icon>
+                            <View />
+                        </el-icon>
+                        <template #title>
+                            Process Monitor
+                        </template>
+                    </el-menu-item>
+                    <el-menu-item index="4">
+                        <el-icon>
+                            <Setting />
+                        </el-icon>
+                        <template #title>
+                            Settings
+                        </template>
+                    </el-menu-item>
+                </el-menu>
+            </el-aside>
+            <el-container>
+                <el-header>
+                    <h2>Xav Admin Management Platform</h2>
+                </el-header>
+                <el-main>
+                    <el-row>
+                        <el-col :span="24">
+                            <el-card>
+                                <template #header>
+                                    <el-icon>
+                                        <Monitor />
+                                    </el-icon>
+                                    <span>&nbsp;&nbsp;Basic Information of {{ machineName }}</span>
+                                </template>
+                                <template #default>
+                                    <el-row>
+                                        <el-col :span="2"></el-col>
+                                        <el-col :span="2">
+                                            <el-icon class="monitor-icon" size="128" :color="successColor">
+                                                <Monitor />
+                                            </el-icon>
+                                        </el-col>
+                                        <el-col :span="1"></el-col>
+                                        <el-col :span="5">
+                                            <el-row class="basic-info">
+                                                <el-text>System: {{ system }}</el-text>
+                                            </el-row>
+                                            <el-row class="basic-info">
+                                                <el-text>Connection Status: </el-text>
+                                                <el-tag type="success" class="basic-info-tag">Online</el-tag>
+                                            </el-row>
+                                            <el-row class="basic-info">
+                                                <el-text>Security Status: </el-text>
+                                                <el-tag type="success" class="basic-info-tag">Safe</el-tag>
+                                            </el-row>
+                                            <el-button type="danger" plain disabled>Disconnect</el-button>
+                                        </el-col>
+                                        <el-col :span="4">
+                                            <el-statistic title="On Access Scanner Scanned Objects"
+                                                :value="onAccessScannerScanObjectCount" />
+                                            <div class="statistic-margin" />
+                                            <el-statistic title="On Access Scanner Blocked Threats"
+                                                :value="onAccessScannerBlockedThreatsCount"
+                                                :value-style="{ color: dangerColor }" />
+                                        </el-col>
+                                        <el-col :span="3">
+                                            <el-statistic title="Total Events" :value="totalEventCount" />
+                                            <div class="statistic-margin" />
+                                            <el-statistic title="Suspicious Events" :value="suspiciousEventCount"
+                                                :value-style="{ color: warningColor }" />
+                                        </el-col>
+                                        <el-col :span="3">
+                                            <el-progress type="dashboard" :percentage="cpuUsage" :color="dangerColor">
+                                                <template #default="{ percentage }">
+                                                    <el-text class="percentage-value">{{ percentage }}%</el-text>
+                                                    <el-text class="percentage-label">CPU Usage</el-text>
                                                 </template>
-                                            </el-table-column>
-                                        </el-table>
-                                    </el-col>
-                                </el-row>
-                                <el-row :style="{ marginTop: '15px' }">
-                                    <el-col :span="20"></el-col>
-                                    <el-col :span="4">
-                                        <el-button type="primary" plain disabled>Quarantine</el-button>
-                                        <el-button type="danger" plain disabled>Ignore</el-button>
-                                    </el-col>
-                                </el-row>
-                            </template>
-                        </el-card>
-                    </el-col>
-                </el-row>
-            </el-main>
+                                            </el-progress>
+                                        </el-col>
+                                        <el-col :span="3">
+                                            <el-progress type="dashboard" :percentage="memoryUsage"
+                                                :color="warningColor">
+                                                <template #default="{ percentage }">
+                                                    <el-text class="percentage-value">{{ percentage }}%</el-text>
+                                                    <el-text class="percentage-label">Memory Usage</el-text>
+                                                </template>
+                                            </el-progress>
+                                        </el-col>
+                                    </el-row>
+                                </template>
+                            </el-card>
+                        </el-col>
+                    </el-row>
+                    <el-row>
+                        <div class="card-margin"></div>
+                    </el-row>
+                    <el-row>
+                        <el-col :span="24">
+                            <el-card>
+                                <template #header>
+                                    <el-icon>
+                                        <Search />
+                                    </el-icon>
+                                    <span>&nbsp;&nbsp;Scan</span>
+                                </template>
+                                <template #default>
+                                    <el-row>
+                                        <el-button plain @click="handleQuickScanBtnClick"
+                                            :disabled="scanButtonDisabled">Quick
+                                            Scan</el-button>
+                                        <el-button plain disabled>Full Scan</el-button>
+                                        <el-button plain disabled>Custom Scan</el-button>
+                                    </el-row>
+                                    <div class="scan-margin"></div>
+                                    <el-row>
+                                        <el-col :span="24">
+                                            <el-progress :percentage="scanProgress"
+                                                :status="scanProgressBarStatus"></el-progress>
+                                        </el-col>
+                                    </el-row>
+                                    <el-row>
+                                        <el-col :span="24">
+                                            <el-text>{{ scanStatusText }}</el-text>
+                                        </el-col>
+                                    </el-row>
+                                    <el-row>
+                                        <el-col :span="24">
+                                            <el-table :data="malwareInfos" :max-height="400">
+                                                <el-table-column prop="file_path" label="Path" />
+                                                <el-table-column prop="malware_name" label="Malware Name">
+                                                    <template #default="scope">
+                                                        <el-text type="danger">{{ scope.row.malware_name }}</el-text>
+                                                    </template>
+                                                </el-table-column>
+                                            </el-table>
+                                        </el-col>
+                                    </el-row>
+                                    <el-row :style="{ marginTop: '15px' }">
+                                        <el-col :span="20"></el-col>
+                                        <el-col :span="4">
+                                            <el-button type="primary" plain disabled>Quarantine</el-button>
+                                            <el-button type="danger" plain disabled>Ignore</el-button>
+                                        </el-col>
+                                    </el-row>
+                                </template>
+                            </el-card>
+                        </el-col>
+                    </el-row>
+                </el-main>
+            </el-container>
         </el-container>
     </div>
 </template>
