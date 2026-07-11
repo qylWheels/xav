c14ef46797883dfab00dec4a79b3500e6687ef7e
qylWheels <command1748165360@126.com>
2026-07-11 23:31:36 +0800

    Refactor Dashboard.vue to enhance layout and update security event display.

--- xavadmin/src/views/Dashboard.vue
+++ xavadmin/src/views/Dashboard.vue
@@ -1,74 +1,68 @@
 <template>
     <div class="layout">
-        <el-container>
-            <el-row>
-                <el-col :span="24">
-                    <el-card>
-                        <template #header>
-                            <el-icon>
-                                <Monitor />
-                            </el-icon>
-                            <span>&nbsp;&nbsp;Basic Information of {{ machineName }}</span>
-                        </template>
-                        <template #default>
-                            <el-row>
-                                <el-col :span="2"></el-col>
-                                <el-col :span="2">
-                                    <el-icon class="monitor-icon" size="128" :color="successColor">
-                                        <Monitor />
-                                    </el-icon>
-                                </el-col>
-                                <el-col :span="1"></el-col>
-                                <el-col :span="5">
-                                    <el-row class="basic-info">
-                                        <el-text>System: {{ system }}</el-text>
-                                    </el-row>
-                                    <el-row class="basic-info">
-                                        <el-text>Connection Status: </el-text>
-                                        <el-tag type="success" class="basic-info-tag">Online</el-tag>
-                                    </el-row>
-                                    <el-row class="basic-info">
-                                        <el-text>Security Status: </el-text>
-                                        <el-tag type="success" class="basic-info-tag">Safe</el-tag>
-                                    </el-row>
-                                    <el-button type="danger" plain disabled>Disconnect</el-button>
-                                </el-col>
-                                <el-col :span="4">
-                                    <el-statistic title="On Access Scanner Scanned Objects"
-                                        :value="onAccessScannerScanObjectCount" />
-                                    <div class="statistic-margin" />
-                                    <el-statistic title="On Access Scanner Blocked Threats"
-                                        :value="onAccessScannerBlockedThreatsCount"
-                                        :value-style="{ color: dangerColor }" />
-                                </el-col>
-                                <el-col :span="3">
-                                    <el-statistic title="Total Events" :value="totalEventCount" />
-                                    <div class="statistic-margin" />
-                                    <el-statistic title="Suspicious Events" :value="suspiciousEventCount"
-                                        :value-style="{ color: warningColor }" />
-                                </el-col>
-                                <el-col :span="3">
-                                    <el-progress type="dashboard" :percentage="cpuUsage" :color="dangerColor">
-                                        <template #default="{ percentage }">
-                                            <el-text class="percentage-value">{{ percentage }}%</el-text>
-                                            <el-text class="percentage-label">CPU Usage</el-text>
-                                        </template>
-                                    </el-progress>
-                                </el-col>
-                                <el-col :span="3">
-                                    <el-progress type="dashboard" :percentage="memoryUsage" :color="warningColor">
-                                        <template #default="{ percentage }">
-                                            <el-text class="percentage-value">{{ percentage }}%</el-text>
-                                            <el-text class="percentage-label">Memory Usage</el-text>
-                                        </template>
-                                    </el-progress>
-                                </el-col>
-                            </el-row>
-                        </template>
-                    </el-card>
-                </el-col>
-            </el-row>
-        </el-container>
+        <el-card>
+            <template #header>
+                <el-icon>
+                    <PieChart />
+                </el-icon>
+                <span>&nbsp;&nbsp;Dashboard</span>
+            </template>
+            <template #default>
+                <el-row>
+                    <el-col :span="1"></el-col>
+                    <el-col :span="4">
+                        <el-icon class="monitor-icon" size="128" :color="successColor">
+                            <Monitor />
+                        </el-icon>
+                    </el-col>
+                    <el-col :span="6">
+                        <el-row class="basic-info">
+                            <el-text>Name: {{ machineName }}</el-text>
+                        </el-row>
+                        <el-row class="basic-info">
+                            <el-text>Connection Status: </el-text>
+                            <el-tag type="success" class="basic-info-tag">Online</el-tag>
+                        </el-row>
+                        <el-row class="basic-info">
+                            <el-text>Security Status: </el-text>
+                            <el-tag type="success" class="basic-info-tag">Safe</el-tag>
+                        </el-row>
+                        <el-button type="danger" plain disabled>Disconnect</el-button>
+                    </el-col>
+                    <el-col :span="6">
+                        <el-statistic title="On Access Scanner Scanned Object(s)"
+                            :value="onAccessScannerScanObjectCount" />
+                        <div class="statistic-margin" />
+                        <el-statistic title="On Access Scanner Blocked Threat(s)"
+                            :value="onAccessScannerBlockedThreatsCount" :value-style="{ color: dangerColor }" />
+                    </el-col>
+                    <el-col :span="3">
+                        <el-statistic title="Behavior(s) Detected" :value="totalBehavCount" />
+                        <div class="statistic-margin" />
+                        <el-statistic title="Suspicious Behavior(s)" :value="suspiciousBehavCount"
+                            :value-style="{ color: warningColor }" />
+                    </el-col>
+                </el-row>
+                <el-row>
+                    <el-divider></el-divider>
+                </el-row>
+                <el-row>
+                    <el-text type="large" tag="b">Recent Security Event(s)</el-text>
+                </el-row>
+                <el-row>
+                    <el-table :data="recentSecurityEvents" height="310">
+                        <el-table-column prop="time" label="Time" width="180" />
+                        <el-table-column prop="category" label="Category" width="180" />
+                        <el-table-column prop="severity" label="Severity" width="100">
+                            <template #default="scope">
+                                <el-tag :type="severityOfEvent(scope.row)">{{ scope.row.severity }}</el-tag>
+                            </template>
+                        </el-table-column>
+                        <el-table-column prop="desc" label="Description" />
+                    </el-table>
+                </el-row>
+            </template>
+        </el-card>
     </div>
 </template>
 
@@ -124,6 +118,33 @@ const system = ref("Linux")
 const cpuUsage = ref(80)
 const memoryUsage = ref(60)
 
+const recentSecurityEvents = ref([
+    {
+        time: '2016-05-03 10:00:00',
+        category: 'Suspicious Behavior',
+        severity: 'Medium',
+        desc: '/bin/bash tries to inject malicious code into /sbin/systemd',
+    },
+    {
+        time: '2016-05-03 10:00:02',
+        category: 'Malware Blocked',
+        severity: 'High',
+        desc: 'On access scanner blocked /bin/bash2, because it is Ransom.Petya.d',
+    }
+])
+recentSecurityEvents.value = recentSecurityEvents.value.flatMap((item) => [item, item, item, item, item, item])
+
+const severityOfEvent = (event: any) => {
+    if (event.severity === 'Medium') {
+        return 'warning'
+    } else if (event.severity === 'High') {
+        return 'danger'
+    } else if (event.severity === 'Low') {
+        return 'info'
+    }
+    return 'primary'
+}
+
 // Real-time protection status.
 const onAccessScannerScanObjectCount = ref(0)
 const onAccessScannerBlockedThreatsCount = ref(0)
@@ -141,8 +162,8 @@ const {
 }, 3000, { immediate: true })
 
 // Behavior monitor status.
-const totalEventCount = ref(0)
-const suspiciousEventCount = ref(0)
+const totalBehavCount = ref(0)
+const suspiciousBehavCount = ref(0)
 const {
     pause: pauseQueryBehaviorMonitorStatus,
     resume: resumeQueryBehaviorMonitorStatus,
@@ -151,8 +172,8 @@ const {
     axios.get('/api/behavior-monitor/status')
         .then((res) => {
             const data = res.data
-            totalEventCount.value = data.total_event_count
-            suspiciousEventCount.value = data.suspicious_event_count
+            totalBehavCount.value = data.total_event_count
+            suspiciousBehavCount.value = data.suspicious_event_count
         })
 }, 3000, { immediate: true })
 </script>
