28f9c8404b6924d16b3e10f4d7411afe6b1032c3
qylWheels <command1748165360@126.com>
2026-07-13 13:35:07 +0800

    Add ExactHashEngine and StaticHeuristicEngineManager to GlobalContext.

--- xavagent/global_context.h
+++ xavagent/global_context.h
@@ -3,6 +3,9 @@
 #include "xavagent/edr/behavioral_protection/behavior_monitor.h"
 #include "xavagent/edr/on_access_scanner.h"
 #include "xavagent/edr/scanner.h"
+#include "xavlib/exact_hash.h"
+#include "xavlib/heuristic/static_heuristic.h"
+#include "xavlib/heuristic/yara_static_heuristic_engine.h"
 
 namespace xavagent {
 class GlobalContext;
@@ -20,8 +23,20 @@ public:
 
     BehaviorMonitor& behavior_monitor() { return this->behavior_monitor_; }
 
+    xavlib::ExactHashEngine& exact_hash_engine() {
+        return this->exact_hash_engine_;
+    }
+
+    xavlib::StaticHeuristicEngineManager& static_heur_engine_manager() {
+        return this->static_heur_engine_manager_;
+    }
+
 private:
-    GlobalContext() = default;
+    GlobalContext() {
+        this->static_heur_engine_manager_.add_engine(
+            std::make_shared<xavlib::YaraStaticHeuristicEngine>());
+    }
+
     ~GlobalContext() = default;
     GlobalContext(const GlobalContext&) = delete;
     GlobalContext& operator=(const GlobalContext&) = delete;
@@ -32,5 +47,7 @@ private:
     Scanner scanner_;
     OnAccessScanner on_access_scanner_;
     BehaviorMonitor behavior_monitor_;
+    xavlib::ExactHashEngine exact_hash_engine_;
+    xavlib::StaticHeuristicEngineManager static_heur_engine_manager_;
 };
 }  // namespace xavagent
