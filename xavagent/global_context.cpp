fd00943f7eec2511e21fe08a5534e16a234bbf2a
qylWheels <command1748165360@126.com>
2026-07-13 14:44:34 +0800

    Add websocket into GlobalContext.

---
+++ xavagent/global_context.cpp
@@ -0,0 +1,28 @@
+#include "global_context.h"
+
+#include "xavlib/heuristic/yara_static_heuristic_engine.h"
+
+namespace http = beast::http;
+
+namespace xavagent {
+GlobalContext::GlobalContext(net::io_context& ioc) : ws_(ioc) {
+    // Websocket configs.
+    std::string host = "0.0.0.0";
+    std::string port = "8001";
+    tcp::resolver resolver{ioc};
+    auto const results = resolver.resolve(host, port);
+    auto ep = net::connect(this->ws_.next_layer(), results);
+    host += ':' + std::to_string(ep.port());
+    this->ws_.set_option(
+        websocket::stream_base::decorator([](websocket::request_type& req) {
+            req.set(http::field::user_agent,
+                    std::string(BOOST_BEAST_VERSION_STRING) +
+                        " websocket-client-coro");
+        }));
+    this->ws_.handshake(host, "/ws");
+
+    // Add Yara static heuristic engine to the manager.
+    this->static_heur_engine_manager_.add_engine(
+        std::make_shared<xavlib::YaraStaticHeuristicEngine>());
+}
+}  // namespace xavagent
--- xavagent/global_context.h
+++ xavagent/global_context.h
@@ -1,19 +1,27 @@
 #pragma once
 
+#include <boost/asio/connect.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/websocket.hpp>
+
 #include "xavagent/edr/behavioral_protection/behavior_monitor.h"
 #include "xavagent/edr/on_access_scanner.h"
 #include "xavagent/edr/scanner.h"
 #include "xavlib/exact_hash.h"
 #include "xavlib/heuristic/static_heuristic.h"
-#include "xavlib/heuristic/yara_static_heuristic_engine.h"
+
+namespace beast = boost::beast;
+namespace websocket = beast::websocket;
+using tcp = boost::asio::ip::tcp;
+namespace net = boost::asio;
 
 namespace xavagent {
-class GlobalContext;
-
 class GlobalContext {
 public:
     static GlobalContext& get_global_context() {
-        static GlobalContext instance;
+        static net::io_context ioc;
+        static GlobalContext instance(ioc);
         return instance;
     }
 
@@ -31,12 +39,10 @@ public:
         return this->static_heur_engine_manager_;
     }
 
-private:
-    GlobalContext() {
-        this->static_heur_engine_manager_.add_engine(
-            std::make_shared<xavlib::YaraStaticHeuristicEngine>());
-    }
+    websocket::stream<tcp::socket>& ws() { return this->ws_; }
 
+private:
+    GlobalContext(net::io_context& ioc);
     ~GlobalContext() = default;
     GlobalContext(const GlobalContext&) = delete;
     GlobalContext& operator=(const GlobalContext&) = delete;
@@ -49,5 +55,6 @@ private:
     BehaviorMonitor behavior_monitor_;
     xavlib::ExactHashEngine exact_hash_engine_;
     xavlib::StaticHeuristicEngineManager static_heur_engine_manager_;
+    websocket::stream<tcp::socket> ws_;
 };
 }  // namespace xavagent
