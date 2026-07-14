9f677eee6d30b4ca4e594c9df5ef3ed5a522a7b3
qylWheels <command1748165360@126.com>
2026-07-14 21:31:19 +0800

    Enhance YARA Forge download script to move and clean up extracted files.

--- xavdb/scripts/get_yara_forge.sh
+++ xavdb/scripts/get_yara_forge.sh
@@ -1,3 +1,5 @@
 wget -P ../db/ https://github.com/YARAHQ/yara-forge/releases/latest/download/yara-forge-rules-extended.zip
 cd ../db/
 unzip yara-forge-rules-extended.zip
+mv packages/extended/yara-rules-extended.yar .
+rm -rf packages/ yara-forge-rules-extended.zip
