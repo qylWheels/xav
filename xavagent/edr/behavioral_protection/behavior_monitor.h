d5a3aa9dceeb31831da5b2cbf70bae860a4076a7
qylWheels <command1748165360@126.com>
2026-07-16 19:47:38 +0800

    Refactor BehaviorMonitor and BehaviorMonitorManager classes for improved structure and clarity.

--- xavagent/edr/behavioral_protection/behavior_monitor.h
+++ xavagent/edr/behavioral_protection/behavior_monitor.h
@@ -1,27 +1,40 @@
 #pragma once
 
 #include <linux/fanotify.h>
-#include <spdlog/spdlog.h>
 
 #include <atomic>
 #include <cstdint>
 #include <deque>
 #include <memory>
-#include <optional>
+#include <span>
 #include <unordered_map>
+#include <vector>
 
-#include "event.h"
-#include "spdlog/logger.h"
+#include "xavagent/edr/behavioral_protection/event.h"
 
 namespace xavagent {
-class BehaviorMonitor {
+class IBehaviorMonitor {
 public:
-    BehaviorMonitor();
-    ~BehaviorMonitor();
-    BehaviorMonitor(const BehaviorMonitor&) = delete;
-    BehaviorMonitor& operator=(const BehaviorMonitor&) = delete;
-    BehaviorMonitor(BehaviorMonitor&&) = delete;
-    BehaviorMonitor& operator=(BehaviorMonitor&&) = delete;
+    virtual ~IBehaviorMonitor() = default;
+
+public:
+    virtual void start_monitoring() = 0;
+    virtual void stop_monitoring() = 0;
+    virtual std::span<Event> all_events() const = 0;
+    virtual std::size_t event_count() const = 0;
+};
+
+class BehaviorMonitorManager {
+public:
+    BehaviorMonitorManager();
+    ~BehaviorMonitorManager();
+    BehaviorMonitorManager(const BehaviorMonitorManager&) = delete;
+    BehaviorMonitorManager& operator=(const BehaviorMonitorManager&) = delete;
+    BehaviorMonitorManager(BehaviorMonitorManager&&) = delete;
+    BehaviorMonitorManager& operator=(BehaviorMonitorManager&&) = delete;
+
+public:
+    void add_behavior_monitor(std::shared_ptr<IBehaviorMonitor> monitor);
 
 public:
     void start_monitoring();
@@ -35,23 +48,9 @@ public:
     }
 
 private:
-    std::optional<std::string> get_path_from_dfid_name_record(
-        fanotify_event_info_fid* dfid_name_record);
-
-private:
-    std::optional<int> get_proc_ppid(int pid);
-    std::optional<unsigned long long> get_proc_start_time_tick(int pid);
-    std::optional<std::string> get_proc_exe_path(int pid);
-    std::optional<std::string> get_proc_cmdline(int pid);
-    std::optional<std::string> get_proc_raw_stat(int pid, int n);
-
-private:
-    int fanfd_;
-    char* fanbuf_;
-    int mount_fd_;
     std::unordered_map<Process, std::deque<Event>> procs_events_;
     std::atomic_uint64_t total_event_count_;
     std::atomic_uint64_t suspicious_event_count_;
-    std::shared_ptr<spdlog::logger> logger_;
+    std::vector<std::shared_ptr<IBehaviorMonitor>> behavior_monitors_;
 };
 }  // namespace xavagent
