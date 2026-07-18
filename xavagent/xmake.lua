73f98f9a1617af174e47a5b88238a69750a842df
qylWheels <command1748165360@126.com>
2026-07-18 14:04:20 +0800

    Refactor include directories in xmake.lua for clarity and consistency.

--- xavagent/xmake.lua
+++ xavagent/xmake.lua
@@ -10,6 +10,8 @@ add_requires(
 add_requires("apt::libyara-dev", {alias = "yara"})
 add_requires("vcpkg::boost-beast", {alias = "boost-beast"})
 
+add_includedirs("$(projectdir)")
+
 target("xavagent")
     set_kind("binary")
     add_files("**.cpp")
@@ -18,6 +20,5 @@ target("xavagent")
         "spdlog", "yaml-cpp", "yara", "outcome", "rapidfuzz", "cpptrace",
         "boost-beast"
     )
-    add_includedirs("./")
 
 includes("edr/behavioral_protection/syscall_monitor/ebpf")
