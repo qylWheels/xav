e17a6a09e51d69268d8ba12cec1299cdd3e59b5c
qylWheels <command1748165360@126.com>
2026-07-26 19:48:00 +0800

    Remove unused BehaviorMonitorManager class and related includes.

--- xavagent/src/include/xavagent/protection/behavior_monitor.h
+++ xavagent/src/include/xavagent/protection/behavior_monitor.h
@@ -2,15 +2,11 @@
 
 #include <linux/fanotify.h>
 
-#include <atomic>
 #include <cstdint>
-#include <deque>
 #include <memory>
 #include <outcome.hpp>
 #include <outcome/config.hpp>
 #include <outcome/result.hpp>
-#include <unordered_map>
-#include <unordered_set>
 
 #include "xavagent/protection/event.h"
 
@@ -45,33 +41,4 @@ public:
     virtual outcome::result<void> accept(const Event& event) = 0;
 };
 
-class BehaviorMonitorManager {
-public:
-    BehaviorMonitorManager();
-    ~BehaviorMonitorManager();
-    BehaviorMonitorManager(const BehaviorMonitorManager&) = delete;
-    BehaviorMonitorManager& operator=(const BehaviorMonitorManager&) = delete;
-    BehaviorMonitorManager(BehaviorMonitorManager&&) = delete;
-    BehaviorMonitorManager& operator=(BehaviorMonitorManager&&) = delete;
-
-public:
-    void add_behavior_monitor(std::shared_ptr<IEventProvider> monitor);
-
-public:
-    void start_monitoring();
-    void stop_monitoring();
-
-public:
-    std::uint64_t total_event_count() const { return this->total_event_count_; }
-
-    std::uint64_t suspicious_event_count() const {
-        return this->suspicious_event_count_;
-    }
-
-private:
-    std::unordered_map<Process, std::deque<Event>> procs_events_;
-    std::atomic_uint64_t total_event_count_;
-    std::atomic_uint64_t suspicious_event_count_;
-    std::unordered_set<std::shared_ptr<IEventProvider>> behavior_monitors_;
-};
 }  // namespace xavagent
