315cf2340680e3661b69778ca68d1f266db65ef7
qylWheels <command1748165360@126.com>
2026-07-26 14:59:41 +0800

    Enhance SyscallMonitor to handle read syscall events and store detailed read information.

--- xavagent/src/protection/event_collection/syscall_monitor/syscall_monitor.cpp
+++ xavagent/src/protection/event_collection/syscall_monitor/syscall_monitor.cpp
@@ -1,18 +1,21 @@
 #include "xavagent/protection/event_collection/syscall_monitor/syscall_monitor.h"
 
+#include <bits/types/struct_iovec.h>
 #include <bpf/libbpf.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/spdlog.h>
 #include <sys/resource.h>
+#include <sys/syscall.h>
+#include <sys/uio.h>
 #include <unistd.h>
 
-#include <algorithm>
 #include <numeric>
 #include <optional>
 #include <pfs/procfs.hpp>
 #include <stdexcept>
 
 #include "syscall_monitor.skel.h"
+#include "xavagent/protection/event.h"
 #include "xavagent/protection/event_collection/syscall_monitor/raw_syscall_event.h"
 
 namespace xavagent {
@@ -67,6 +70,13 @@ SyscallMonitor::all_events_of_procs() const {
 int SyscallMonitor::event_handler(void* ctx, void* data, std::size_t size) {
     SyscallMonitor* self = static_cast<SyscallMonitor*>(ctx);
     RawSyscallEvent* e = static_cast<RawSyscallEvent*>(data);
+
+    // TODO: We only monitor read syscall for now.
+    if (e->syscall_id != SYS_read) {
+        return 0;
+    }
+
+    // Parse process info.
     Process proc{.pid = e->pid};
     try {
         proc.ppid = pfs::procfs().get_task(e->pid).get_stat().ppid;
@@ -92,7 +102,40 @@ int SyscallMonitor::event_handler(void* ctx, void* data, std::size_t size) {
     } catch (...) {
         proc.cmdline = std::nullopt;
     }
-    self->events_[proc].push_back(*e);
+
+    // Parse syscall info.
+    ReadSyscallEvent read_event;
+    read_event.fd = e->args[0];
+    read_event.buf = reinterpret_cast<void*>(e->args[1]);
+    read_event.count = e->args[2];
+    read_event.ret = e->ret;
+    try {
+        read_event.path = pfs::procfs()
+                              .get_task(e->pid)
+                              .get_fds()[read_event.fd]
+                              .get_target();
+    } catch (...) {
+        read_event.path = std::nullopt;
+    }
+    try {
+        std::vector<char> buf_content(read_event.count, 0);
+        iovec local, remote;
+        local.iov_base = buf_content.data();
+        local.iov_len = read_event.count;
+        remote.iov_base = read_event.buf;
+        remote.iov_len = read_event.count;
+        read_event.buf_content = buf_content;
+        ssize_t ret = process_vm_readv(e->pid, &local, 1, &remote, 1, 0);
+        if (ret == -1) {
+            read_event.buf_content = std::nullopt;
+        }
+    } catch (...) {
+        read_event.buf_content = std::nullopt;
+    }
+
+    // Store event.
+    self->events_[proc].push_back(read_event);
+
     return 0;
 }
 }  // namespace xavagent
