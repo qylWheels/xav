6d6d0bc3c3fcb8e06f114dda2ff9ee17909e0cac
qylWheels <command1748165360@126.com>
2026-07-28 00:34:08 +0800

    Implement NormalScanStrategy for multi-engine scanning support.

---
+++ xavagent/src/include/xavagent/scan/normal_scan_strategy.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "xavagent/scan/scan_interfaces.h"
+
+namespace xavagent {
+class NormalScanStrategy : public IScanStrategy {
+public:
+    NormalScanStrategy(IScanEngine &exact_hash_engine,
+                       IScanEngine &yara_static_heuristic_engine);
+    ~NormalScanStrategy() = default;
+
+public:
+    virtual outcome::result<
+        std::vector<outcome::result<std::optional<malware_info::MalwareInfo>>>>
+    scan(const std::filesystem::path &path) override;
+
+private:
+    IScanEngine *exact_hash_engine_;
+    IScanEngine *yara_static_heuristic_engine_;
+};
+}  // namespace xavagent
---
+++ xavagent/src/scan/normal_scan_strategy.cpp
@@ -0,0 +1,25 @@
+#include "xavagent/scan/normal_scan_strategy.h"
+
+#include <vector>
+
+namespace xavagent {
+NormalScanStrategy::NormalScanStrategy(
+    IScanEngine& exact_hash_engine, IScanEngine& yara_static_heuristic_engine)
+    : exact_hash_engine_(&exact_hash_engine),
+      yara_static_heuristic_engine_(&yara_static_heuristic_engine) {}
+
+outcome::result<
+    std::vector<outcome::result<std::optional<malware_info::MalwareInfo>>>>
+NormalScanStrategy::scan(const std::filesystem::path& path) {
+    std::vector<outcome::result<std::optional<malware_info::MalwareInfo>>>
+        results;
+
+    auto result_from_exact_hash_engine = this->exact_hash_engine_->scan(path);
+    results.push_back(result_from_exact_hash_engine);
+    auto result_from_heur_engine =
+        this->yara_static_heuristic_engine_->scan(path);
+    results.push_back(result_from_heur_engine);
+
+    return results;
+}
+}  // namespace xavagent
