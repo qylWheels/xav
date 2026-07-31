c9cb3f0eacfc344c516ba2ea7217c0f870e6b726
qylWheels <command1748165360@126.com>
2026-07-31 14:06:06 +0800

    Refactor RawSyscallEvent structure to use standard types and improve data packing.

--- xavcore/src/include/xavcore/protection/event_provider/syscall_monitor/raw_syscall_event.h
+++ xavcore/src/include/xavcore/protection/event_provider/syscall_monitor/raw_syscall_event.h
@@ -1,11 +1,23 @@
 #pragma once
 
-struct RawSyscallEvent {
-    char enter_captured;
-    char exit_captured;
+#if defined(__BPF__) || defined(__bpf__)
+#include "vmlinux.h"
+typedef __u8 u8;
+typedef __u32 u32;
+typedef __u64 u64;
+#else
+#include <cstdint>
+typedef std::uint8_t u8;
+typedef std::uint32_t u32;
+typedef std::uint64_t u64;
+#endif  // __BPF__
 
-    long long pid;
-    long int syscall_id;
-    long unsigned int args[6];
-    long ret;
+struct __attribute__((packed)) RawSyscallEvent {
+    u8 enter_captured;
+    u8 exit_captured;
+
+    u32 pid;
+    u32 syscall_id;
+    u64 args[6];
+    u64 ret;
 };
