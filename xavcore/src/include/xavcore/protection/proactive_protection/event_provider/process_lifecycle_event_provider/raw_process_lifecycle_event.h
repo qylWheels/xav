fdef0888b0f38164e7d5eebf063ed019fe2cc4cf
qylWheels <command1748165360@126.com>
2026-08-02 12:54:08 +0800

    Add RawProcessLifecycleEvent struct for process lifecycle tracking.

--- xavcore/src/include/xavcore/protection/proactive_protection/event_provider/process_lifecycle_event_provider/raw_process_lifecycle_event.h
+++ xavcore/src/include/xavcore/protection/proactive_protection/event_provider/process_lifecycle_event_provider/raw_process_lifecycle_event.h
@@ -2,9 +2,11 @@
 
 #if defined(__BPF__) || defined(__bpf__)
 #include "vmlinux.h"
+typedef __u8 u8;
 typedef __u32 u32;
 #else
 #include <cstdint>
+typedef std::uint8_t u8;
 typedef std::uint32_t u32;
 #endif  // __BPF__, __bpf__
 
@@ -16,14 +18,26 @@ struct __attribute__((packed)) RawProcessExitEvent {
     u32 pid;
 };
 
+struct __attribute__((packed)) RawProcessLifecycleEvent {
+    u8 tag;  // 0 = create, 1 = exit
+    union {
+        RawProcessCreateEvent create;
+        RawProcessExitEvent exit;
+    } u;
+};
+
 #ifdef __cplusplus
 static_assert(sizeof(RawProcessCreateEvent) == 4,
               "RawProcessCreateEvent size is not 4 bytes");
 static_assert(sizeof(RawProcessExitEvent) == 4,
               "RawProcessExitEvent size is not 4 bytes");
+static_assert(sizeof(RawProcessLifecycleEvent) == 5,
+              "RawProcessLifecycleEvent size is not 5 bytes");
 #else
 _Static_assert(sizeof(struct RawProcessCreateEvent) == 4,
                "RawProcessCreateEvent size is not 4 bytes");
 _Static_assert(sizeof(struct RawProcessExitEvent) == 4,
                "RawProcessExitEvent size is not 4 bytes");
+_Static_assert(sizeof(struct RawProcessLifecycleEvent) == 5,
+               "RawProcessLifecycleEvent size is not 5 bytes");
 #endif  // __cplusplus
