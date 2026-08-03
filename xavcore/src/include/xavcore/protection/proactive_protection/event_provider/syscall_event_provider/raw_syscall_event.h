bdeb8a24560d206fb4a28021162186c56b7959c8
qylWheels <command1748165360@126.com>
2026-08-03 14:50:46 +0800

    Add RawReadSyscallEvent in raw_syscall_event.h.

--- xavcore/src/include/xavcore/protection/proactive_protection/event_provider/syscall_event_provider/raw_syscall_event.h
+++ xavcore/src/include/xavcore/protection/proactive_protection/event_provider/syscall_event_provider/raw_syscall_event.h
@@ -5,11 +5,15 @@
 typedef __u8 u8;
 typedef __u32 u32;
 typedef __u64 u64;
+typedef __s32 i32;
+typedef __s64 i64;
 #else
 #include <cstdint>
 typedef std::uint8_t u8;
 typedef std::uint32_t u32;
 typedef std::uint64_t u64;
+typedef std::int32_t i32;
+typedef std::int64_t i64;
 #endif  // __BPF__, __bpf__
 
 struct __attribute__((packed)) RawSyscallEvent {
@@ -29,3 +33,17 @@ static_assert(sizeof(RawSyscallEvent) == 66,
 _Static_assert(sizeof(struct RawSyscallEvent) == 66,
                "RawSyscallEvent size is not 66 bytes");
 #endif  // __cplusplus
+
+struct __attribute__((packed)) RawReadSyscallEvent {
+    u8 enter_captured;
+    u8 exit_captured;
+
+    u32 pid;
+
+    i32 fd;
+    u64 buf;
+    u64 count;
+    i64 ret;
+
+    u8 path[];
+};
