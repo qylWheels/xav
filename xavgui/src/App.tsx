01e0742308140b92d0aeede0e769d2229aa5fbba
qylWheels <command1748165360@126.com>
2026-08-04 00:08:43 +0800

    Implement rounded corner and shadow for window.

--- xavgui/src/App.tsx
+++ xavgui/src/App.tsx
@@ -6,18 +6,20 @@ import TitleBar from "@/components/TitleBar"
 
 function App({ children }: { children: React.ReactNode }) {
   return (
-    <div className="w-full h-full">
-      <TitleBar />
-      <SidebarProvider style={
-        {
-          "--sidebar-width": "12rem",
-        } as React.CSSProperties
-      }>
-        <AppSidebar />
-        <main>
-          {children}
-        </main>
-      </SidebarProvider>
+    <div className="w-screen h-screen bg-transparent p-4 overflow-hidden">
+      <div className="w-full h-full bg-white rounded-xl shadow-lg">
+        <TitleBar />
+        <SidebarProvider style={
+          {
+            "--sidebar-width": "12rem",
+          } as React.CSSProperties
+        }>
+          <AppSidebar />
+          <main>
+            {children}
+          </main>
+        </SidebarProvider>
+      </div>
     </div>
   );
 }
