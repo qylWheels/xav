ec587436b787f8c710559b73c758a46ad3d818b2
qylWheels <command1748165360@126.com>
2026-08-07 20:40:42 +0800

    Add HipsMonitor class for monitoring events in HIPS protection module.

---
+++ xavcore/src/include/xavcore/protection/hips/monitor/hips_monitor.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <spdlog/logger.h>
+#include <spdlog/spdlog.h>
+
+#include <outcome/config.hpp>
+#include <outcome/outcome.hpp>
+
+#include "hips_monitor.skel.h"
+
+namespace outcome = OUTCOME_V2_NAMESPACE;
+
+namespace xavcore {
+class HipsMonitor {
+public:
+    HipsMonitor(spdlog::logger& logger);
+    ~HipsMonitor();
+    HipsMonitor(const HipsMonitor&) = delete;
+    HipsMonitor& operator=(const HipsMonitor&) = delete;
+    HipsMonitor(HipsMonitor&&) = delete;
+    HipsMonitor& operator=(HipsMonitor&&) = delete;
+
+public:
+    outcome::result<void> start();
+    outcome::result<void> stop();
+
+private:
+    static int event_callback(void* ctx, void* data, std::size_t size);
+
+private:
+    enum class Status { Stopped, Running };
+
+    Status status_;
+    spdlog::logger* logger_;
+    hips_monitor_bpf* skel_;
+    ring_buffer* rb_;
+    std::jthread monitor_thread_;
+};
+}  // namespace xavcore
