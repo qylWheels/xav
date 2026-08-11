306f8e0c4549e98234612a7540d83c141b2710e3
qylWheels <command1748165360@126.com>
2026-08-11 21:14:18 +0800

    Add AnomalousSyscallDetectionListener class for event handling.

---
+++ xavcore/src/include/xavcore/protection/proactive_protection/event_listener/anomalous_syscall_detection_listener/anomalous_syscall_detection_listener.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <spdlog/logger.h>
+
+#include "xavcore/protection/proactive_protection/behavior_monitor.h"
+
+namespace xavcore {
+class AnomalousSyscallDetectionListener : public IEventListener {
+public:
+    AnomalousSyscallDetectionListener(spdlog::logger &logger);
+    ~AnomalousSyscallDetectionListener();
+    AnomalousSyscallDetectionListener(
+        const AnomalousSyscallDetectionListener &) = delete;
+    AnomalousSyscallDetectionListener &operator=(
+        const AnomalousSyscallDetectionListener &) = delete;
+
+    AnomalousSyscallDetectionListener(AnomalousSyscallDetectionListener &&) =
+        delete;
+    AnomalousSyscallDetectionListener &operator=(
+        AnomalousSyscallDetectionListener &&) = delete;
+
+public:
+    virtual bool is_accept(const IEvent &event) override;
+    virtual outcome::result<void> accept(const IEvent &event) override;
+};
+}  // namespace xavcore
