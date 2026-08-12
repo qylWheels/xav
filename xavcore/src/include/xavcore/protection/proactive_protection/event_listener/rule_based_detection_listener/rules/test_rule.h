7760de00a07112d232820977f0fdb05c0740852d
qylWheels <command1748165360@126.com>
2026-08-12 22:05:27 +0800

    Add FSM structure to TestRule for state management in event processing.

--- xavcore/src/include/xavcore/protection/proactive_protection/event_listener/rule_based_detection_listener/rules/test_rule.h
+++ xavcore/src/include/xavcore/protection/proactive_protection/event_listener/rule_based_detection_listener/rules/test_rule.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <boost/sml.hpp>
+
 #include "xavcore/protection/proactive_protection/event_listener/rule_based_detection_listener/rule_interfaces.h"
 
 namespace xavcore {
@@ -16,6 +18,27 @@ public:
 public:
     virtual outcome::result<std::uint8_t> apply(
         std::span<std::reference_wrapper<IEvent>> event_seq) override;
+
+private:
+    struct FSM {
+        // States.
+        struct Start {};
+        struct Rename1 {};
+        struct Rename2 {};
+        struct Rename3 {};
+        struct End {};
+
+        // Events.
+        struct Rename {};
+
+        auto operator()() const {
+            return boost::sml::make_transition_table(
+                *Start + event<Rename> = Rename1,
+                Rename1 + event<Rename> = Rename2,
+                Rename2 + event<Rename> = Rename3,
+                Rename3 + event<Rename> = End, End + event<Rename> = End);
+        }
+    };
 };
 }  // namespace proactive_protection_rule
 }  // namespace xavcore
