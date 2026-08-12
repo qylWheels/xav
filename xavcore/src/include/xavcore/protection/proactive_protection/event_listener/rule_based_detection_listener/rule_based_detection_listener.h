fc668372282c518878816c62e58a6f40e4413873
qylWheels <command1748165360@126.com>
2026-08-12 20:10:40 +0800

    Add RuleBasedDetectionListener class for proactive protection event handling.

---
+++ xavcore/src/include/xavcore/protection/proactive_protection/event_listener/rule_based_detection_listener/rule_based_detection_listener.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <spdlog/logger.h>
+
+#include "xavcore/protection/proactive_protection/behavior_monitor.h"
+#include "xavcore/protection/proactive_protection/event_listener/rule_based_detection_listener/rule_interfaces.h"
+
+namespace xavcore {
+class RuleBasedDetectionListener : public IEventListener {
+public:
+    RuleBasedDetectionListener(spdlog::logger& logger);
+    ~RuleBasedDetectionListener();
+    RuleBasedDetectionListener(const RuleBasedDetectionListener&) = delete;
+    RuleBasedDetectionListener& operator=(const RuleBasedDetectionListener&) =
+        delete;
+    RuleBasedDetectionListener(RuleBasedDetectionListener&&) = delete;
+    RuleBasedDetectionListener& operator=(RuleBasedDetectionListener&&) =
+        delete;
+
+public:  // IEventListener interface methods.
+    virtual bool is_accept(const IEvent& event) override;
+    virtual outcome::result<void> accept(const IEvent& event) override;
+
+public:
+    outcome::result<void> add_rule(IProactiveProtectionRule& rule);
+    outcome::result<void> remove_rule(IProactiveProtectionRule& rule);
+};
+}  // namespace xavcore
