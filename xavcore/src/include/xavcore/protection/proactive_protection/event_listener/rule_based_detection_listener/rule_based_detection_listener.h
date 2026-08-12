1ee97064426b2e227e1beeffc1c45a04049491c8
qylWheels <command1748165360@126.com>
2026-08-12 20:27:53 +0800

    Implement RuleBasedDetectionListener class with event acceptance logic and rule management.

--- xavcore/src/include/xavcore/protection/proactive_protection/event_listener/rule_based_detection_listener/rule_based_detection_listener.h
+++ xavcore/src/include/xavcore/protection/proactive_protection/event_listener/rule_based_detection_listener/rule_based_detection_listener.h
@@ -2,6 +2,8 @@
 
 #include <spdlog/logger.h>
 
+#include <unordered_set>
+
 #include "xavcore/protection/proactive_protection/behavior_monitor.h"
 #include "xavcore/protection/proactive_protection/event_listener/rule_based_detection_listener/rule_interfaces.h"
 
@@ -24,5 +26,9 @@ public:  // IEventListener interface methods.
 public:
     outcome::result<void> add_rule(IProactiveProtectionRule& rule);
     outcome::result<void> remove_rule(IProactiveProtectionRule& rule);
+
+private:
+    spdlog::logger* logger_;
+    std::unordered_set<IProactiveProtectionRule*> rules_;
 };
 }  // namespace xavcore
---
+++ xavcore/src/protection/proactive_protection/event_provider/rule_based_detection_listener/rule_based_detection_listener.cpp
@@ -0,0 +1,36 @@
+#include "xavcore/protection/proactive_protection/event_listener/rule_based_detection_listener/rule_based_detection_listener.h"
+
+#include <spdlog/spdlog.h>
+
+#include "xavcore/protection/proactive_protection/event_provider/syscall_event_provider/syscall_event.h"
+
+namespace xavcore {
+RuleBasedDetectionListener::RuleBasedDetectionListener(spdlog::logger& logger)
+    : logger_(&logger) {}
+
+RuleBasedDetectionListener::~RuleBasedDetectionListener() = default;
+
+bool RuleBasedDetectionListener::is_accept(const IEvent& event) {
+    try {
+        (void)dynamic_cast<const SyscallEvent&>(event);
+        return true;
+    } catch (...) {
+        return false;
+    }
+}
+
+outcome::result<void> RuleBasedDetectionListener::accept(const IEvent& event) {}
+
+outcome::result<void> RuleBasedDetectionListener::add_rule(
+    IProactiveProtectionRule& rule) {
+    this->rules_.insert(&rule);
+    return outcome::success();
+}
+
+outcome::result<void> RuleBasedDetectionListener::remove_rule(
+    IProactiveProtectionRule& rule) {
+    this->rules_.erase(&rule);
+    return outcome::success();
+}
+
+}  // namespace xavcore
