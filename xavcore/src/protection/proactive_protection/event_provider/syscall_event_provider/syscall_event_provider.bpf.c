b8a15987a52d384a0b194ce4d20538bbaa9697b9
qylWheels <command1748165360@126.com>
2026-08-16 22:14:34 +0800

    Refactor syscall exit handling to correctly manage additional data count for read syscall and ensure proper cleanup in case of errors.

--- xavcore/src/protection/proactive_protection/event_provider/syscall_event_provider/syscall_event_provider.bpf.c
+++ xavcore/src/protection/proactive_protection/event_provider/syscall_event_provider/syscall_event_provider.bpf.c
@@ -88,56 +88,71 @@ int trace_sys_exit(struct trace_event_raw_sys_exit* ctx) {
         bpf_ringbuf_output(&rb, &e2, sizeof(e2), 0);
         return 0;
     }
+
     e->exit_captured = 1;
     e->ret = ctx->ret;
-    e->additional_data_count = 0;
 
     // Handle specific syscalls who have additional data. i.e. read.
     switch (e->syscall_id) {
         case 0: {  // read.
-            e->additional_data_count = 1;
             u32 zero = 0;
             u8* pathbuf0 = (u8*)bpf_map_lookup_elem(&pathbufs, &zero);
             if (!pathbuf0) {
+                e->additional_data_count = 0;
+                bpf_ringbuf_output(&rb, e, sizeof(*e), 0);
                 break;
             }
+
             int result = bpf_xavcore_fd_to_path_str((char*)pathbuf0,
                                                     MAX_PATH_LEN, e->args[0]);
             if (result != 0) {
+                e->additional_data_count = 0;
+                bpf_ringbuf_output(&rb, e, sizeof(*e), 0);
                 break;
             }
+
             u64 len = bpf_xavcore_strlen((const char*)pathbuf0);
             len = (len > MAX_PATH_LEN) ? MAX_PATH_LEN : len;
-            e->additional_data_lens[0] = 0;
+
             struct bpf_dynptr dynptr;
             result = bpf_ringbuf_reserve_dynptr(&rb, sizeof(*e) + len + 5, 0,
                                                 &dynptr);
             if (result != 0) {
                 bpf_ringbuf_discard_dynptr(&dynptr, 0);
+                e->additional_data_count = 0;
+                bpf_ringbuf_output(&rb, e, sizeof(*e), 0);
                 break;
             }
+
             result = bpf_dynptr_write(&dynptr, sizeof(*e), (void*)pathbuf0,
                                       len + 1, 0);
             if (result != 0) {
                 bpf_ringbuf_discard_dynptr(&dynptr, 0);
+                e->additional_data_count = 0;
+                bpf_ringbuf_output(&rb, e, sizeof(*e), 0);
                 break;
             }
+
+            e->additional_data_count = 1;
             e->additional_data_lens[0] = len + 1;  // Include '\0'.
             result = bpf_dynptr_write(&dynptr, 0, e, sizeof(*e), 0);
             if (result != 0) {
                 bpf_ringbuf_discard_dynptr(&dynptr, 0);
-                e->additional_data_lens[0] = 0;
+                e->additional_data_count = 0;
+                bpf_ringbuf_output(&rb, e, sizeof(*e), 0);
                 break;
             }
+
             bpf_ringbuf_submit_dynptr(&dynptr, 0);
-            bpf_map_delete_elem(&raw_syscall_event_map, &tid);
             return 0;
         }
-        default:
+        default: {
+            e->additional_data_count = 0;
+            bpf_ringbuf_output(&rb, e, sizeof(*e), 0);
             break;
+        }
     }
 
-    bpf_ringbuf_output(&rb, e, sizeof(*e), 0);
     bpf_map_delete_elem(&raw_syscall_event_map, &tid);
     return 0;
 }
