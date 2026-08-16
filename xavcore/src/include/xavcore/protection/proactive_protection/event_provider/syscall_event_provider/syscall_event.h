850a7f0b54fb3567cc41edccaedd8d9d52b0f8d0
qylWheels <command1748165360@126.com>
2026-08-16 22:55:20 +0800

    Add ReadSyscallAdditionalData structure and update SyscallEvent to include additional data variant.

--- xavcore/src/include/xavcore/protection/proactive_protection/event_provider/syscall_event_provider/syscall_event.h
+++ xavcore/src/include/xavcore/protection/proactive_protection/event_provider/syscall_event_provider/syscall_event.h
@@ -3,12 +3,18 @@
 #include <chrono>
 #include <cstdint>
 #include <filesystem>
+#include <optional>
+#include <string>
 #include <variant>
 #include <vector>
 
 #include "xavcore/protection/proactive_protection/event.h"
 
 namespace xavcore {
+struct ReadSyscallAdditionalData {
+    std::optional<std::string> fd_path;
+};
+
 // Normal syscall event, for those who don't need to parse arguments.
 struct SyscallEvent : public IEvent {
     std::chrono::time_point<std::chrono::system_clock> timestamp;
@@ -16,6 +22,9 @@ struct SyscallEvent : public IEvent {
     std::uint32_t id;
     std::vector<std::uint64_t> args;
     std::uint64_t ret;
+
+    // Additional data.
+    std::variant<std::monostate, ReadSyscallAdditionalData> additional_data;
 };
 
 struct ReadSyscallEventArgs {
