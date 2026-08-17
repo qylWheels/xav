c848dc7aafd2eb08b56a7461b02a7f9095c006f8
qylWheels <command1748165360@126.com>
2026-08-18 00:04:47 +0800

    Only print log when path matches xavcoretest_regex.

--- xavcore/src/protection/proactive_protection/event_listener/rule_based_detection_listener/rule_based_detection_listener.cpp
+++ xavcore/src/protection/proactive_protection/event_listener/rule_based_detection_listener/rule_based_detection_listener.cpp
@@ -4,6 +4,7 @@
 
 #include <algorithm>
 #include <cstddef>
+#include <regex>
 #include <variant>
 #include <vector>
 
@@ -30,76 +31,90 @@ outcome::result<void> RuleBasedDetectionListener::accept(const IEvent& event) {
     this->proc_syscall_events_[syscall_event.process].push_back(syscall_event);
 
     // FIXME: Test print.
-    static int i = 0;
-    while (i++ % 500 == 0) {
-        if (syscall_event.id == SYS_read &&
-            std::holds_alternative<ReadSyscallAdditionalData>(
-                syscall_event.additional_data)) {
-            auto& read_syscall_additional_data =
-                std::get<ReadSyscallAdditionalData>(
-                    syscall_event.additional_data);
-            if (syscall_event.args.empty()) {
-                this->logger_->info("read() = {}", syscall_event.ret);
-            } else {
+    std::regex xavcoretest_regex(".*xavcoretest");
+    if (syscall_event.id == SYS_read &&
+        std::holds_alternative<ReadSyscallAdditionalData>(
+            syscall_event.additional_data)) {
+        auto& read_syscall_additional_data =
+            std::get<ReadSyscallAdditionalData>(syscall_event.additional_data);
+        if (syscall_event.args.empty()) {
+            this->logger_->info("read() = {}", syscall_event.ret);
+        } else {
+            if (read_syscall_additional_data.fd_path.has_value() &&
+                std::regex_match(read_syscall_additional_data.fd_path.value(),
+                                 xavcoretest_regex)) {
                 this->logger_->info(
                     "read({}({}), {:#x}, {}) = {}", syscall_event.args[0],
                     read_syscall_additional_data.fd_path.value_or("<unknown>"),
                     syscall_event.args[1], syscall_event.args[2],
                     syscall_event.ret);
             }
-        } else if (syscall_event.id == SYS_write &&
-                   std::holds_alternative<WriteSyscallAdditionalData>(
-                       syscall_event.additional_data)) {
-            auto& write_syscall_additional_data =
-                std::get<WriteSyscallAdditionalData>(
-                    syscall_event.additional_data);
-            if (syscall_event.args.empty()) {
-                this->logger_->info("write() = {}", syscall_event.ret);
-            } else {
+        }
+    } else if (syscall_event.id == SYS_write &&
+               std::holds_alternative<WriteSyscallAdditionalData>(
+                   syscall_event.additional_data)) {
+        auto& write_syscall_additional_data =
+            std::get<WriteSyscallAdditionalData>(syscall_event.additional_data);
+        if (syscall_event.args.empty()) {
+            this->logger_->info("write() = {}", syscall_event.ret);
+        } else {
+            if (write_syscall_additional_data.fd_path.has_value() &&
+                std::regex_match(write_syscall_additional_data.fd_path.value(),
+                                 xavcoretest_regex)) {
                 this->logger_->info(
                     "write({}({}), {:#x}, {}) = {}", syscall_event.args[0],
                     write_syscall_additional_data.fd_path.value_or("<unknown>"),
                     syscall_event.args[1], syscall_event.args[2],
                     syscall_event.ret);
             }
-        } else if (syscall_event.id == SYS_open &&
-                   std::holds_alternative<OpenSyscallAdditionalData>(
-                       syscall_event.additional_data)) {
-            auto& open_syscall_additional_data =
-                std::get<OpenSyscallAdditionalData>(
-                    syscall_event.additional_data);
-            if (syscall_event.args.empty()) {
-                this->logger_->info("open() = {}", syscall_event.ret);
-            } else {
+        }
+    } else if (syscall_event.id == SYS_open &&
+               std::holds_alternative<OpenSyscallAdditionalData>(
+                   syscall_event.additional_data)) {
+        auto& open_syscall_additional_data =
+            std::get<OpenSyscallAdditionalData>(syscall_event.additional_data);
+        if (syscall_event.args.empty()) {
+            this->logger_->info("open() = {}", syscall_event.ret);
+        } else {
+            if (open_syscall_additional_data.path.has_value() &&
+                std::regex_match(open_syscall_additional_data.path.value(),
+                                 xavcoretest_regex)) {
                 this->logger_->info(
                     "open({:#x}({}), {}, {}) = {}", syscall_event.args[0],
                     open_syscall_additional_data.path.value_or("<unknown>"),
                     syscall_event.args[1], syscall_event.args[2],
                     syscall_event.ret);
             }
-        } else if (syscall_event.id == SYS_close &&
-                   std::holds_alternative<CloseSyscallAdditionalData>(
-                       syscall_event.additional_data)) {
-            auto& close_syscall_additional_data =
-                std::get<CloseSyscallAdditionalData>(
-                    syscall_event.additional_data);
-            if (syscall_event.args.empty()) {
-                this->logger_->info("close() = {}", syscall_event.ret);
-            } else {
+        }
+    } else if (syscall_event.id == SYS_close &&
+               std::holds_alternative<CloseSyscallAdditionalData>(
+                   syscall_event.additional_data)) {
+        auto& close_syscall_additional_data =
+            std::get<CloseSyscallAdditionalData>(syscall_event.additional_data);
+        if (syscall_event.args.empty()) {
+            this->logger_->info("close() = {}", syscall_event.ret);
+        } else {
+            if (close_syscall_additional_data.fd_path.has_value() &&
+                std::regex_match(close_syscall_additional_data.fd_path.value(),
+                                 xavcoretest_regex)) {
                 this->logger_->info(
                     "close({}({})) = {}", syscall_event.args[0],
                     close_syscall_additional_data.fd_path.value_or("<unknown>"),
                     syscall_event.ret);
             }
-        } else if (syscall_event.id == SYS_unlink &&
-                   std::holds_alternative<UnlinkSyscallAdditionalData>(
-                       syscall_event.additional_data)) {
-            auto& unlink_syscall_additional_data =
-                std::get<UnlinkSyscallAdditionalData>(
-                    syscall_event.additional_data);
-            if (syscall_event.args.empty()) {
-                this->logger_->info("unlink() = {}", syscall_event.ret);
-            } else {
+        }
+    } else if (syscall_event.id == SYS_unlink &&
+               std::holds_alternative<UnlinkSyscallAdditionalData>(
+                   syscall_event.additional_data)) {
+        auto& unlink_syscall_additional_data =
+            std::get<UnlinkSyscallAdditionalData>(
+                syscall_event.additional_data);
+        if (syscall_event.args.empty()) {
+            this->logger_->info("unlink() = {}", syscall_event.ret);
+        } else {
+            if (unlink_syscall_additional_data.path.has_value() &&
+                std::regex_match(unlink_syscall_additional_data.path.value(),
+                                 xavcoretest_regex)) {
                 this->logger_->info(
                     "unlink({:#x}({})) = {}", syscall_event.args[0],
                     unlink_syscall_additional_data.path.value_or("<unknown>"),
