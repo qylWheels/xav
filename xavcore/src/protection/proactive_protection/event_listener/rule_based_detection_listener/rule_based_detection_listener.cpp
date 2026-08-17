b9d782bd98ee31a22ed8c8e6a6a0981064e91c19
qylWheels <command1748165360@126.com>
2026-08-17 14:00:45 +0800

    Add test logging for SYS_write syscalls in RuleBasedDetectionListener.

--- xavcore/src/protection/proactive_protection/event_listener/rule_based_detection_listener/rule_based_detection_listener.cpp
+++ xavcore/src/protection/proactive_protection/event_listener/rule_based_detection_listener/rule_based_detection_listener.cpp
@@ -30,19 +30,35 @@ outcome::result<void> RuleBasedDetectionListener::accept(const IEvent& event) {
     this->proc_syscall_events_[syscall_event.process].push_back(syscall_event);
 
     // FIXME: Test print.
-    if (syscall_event.id == SYS_read &&
-        std::holds_alternative<ReadSyscallAdditionalData>(
-            syscall_event.additional_data)) {
-        auto& read_syscall_additional_data =
-            std::get<ReadSyscallAdditionalData>(syscall_event.additional_data);
-        static int i = 0;
-        while (i++ % 400 == 0) {
+    static int i = 0;
+    while (i++ % 500 == 0) {
+        if (syscall_event.id == SYS_read &&
+            std::holds_alternative<ReadSyscallAdditionalData>(
+                syscall_event.additional_data)) {
+            auto& read_syscall_additional_data =
+                std::get<ReadSyscallAdditionalData>(
+                    syscall_event.additional_data);
             if (syscall_event.args.empty()) {
                 this->logger_->info("read() = {}", syscall_event.ret);
             } else {
                 this->logger_->info(
                     "read({}({}), {:#x}, {}) = {}", syscall_event.args[0],
                     read_syscall_additional_data.fd_path.value_or("<unknown>"),
+                    syscall_event.args[1], syscall_event.args[2],
+                    syscall_event.ret);
+            }
+        } else if (syscall_event.id == SYS_write &&
+                   std::holds_alternative<WriteSyscallAdditionalData>(
+                       syscall_event.additional_data)) {
+            auto& write_syscall_additional_data =
+                std::get<WriteSyscallAdditionalData>(
+                    syscall_event.additional_data);
+            if (syscall_event.args.empty()) {
+                this->logger_->info("write() = {}", syscall_event.ret);
+            } else {
+                this->logger_->info(
+                    "write({}({}), {:#x}, {}) = {}", syscall_event.args[0],
+                    write_syscall_additional_data.fd_path.value_or("<unknown>"),
                     syscall_event.args[1], syscall_event.args[2],
                     syscall_event.ret);
             }
