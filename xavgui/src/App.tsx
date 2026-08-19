70c8a31db7ab4f81a2a697224616b5f44c9b48c9
qylWheels <command1748165360@126.com>
2026-08-19 12:14:58 +0800

    Implement menubar in App.tsx.

--- xavgui/src/App.tsx
+++ xavgui/src/App.tsx
@@ -1,11 +1,38 @@
 import "@/index.css"
-import "./App.css"
+import "@/App.css"
 import Overview from "@/views/Overview";
 import Scan from "@/views/Scan";
+import { Shield, Search, LayoutDashboard, Bolt } from "lucide-react";
 
-function App({ children }: { children: React.ReactNode }) {
+function App() {
+  const menuItems = [
+    { icon: LayoutDashboard, text: "Overview" },
+    { icon: Search, text: "Scan" },
+    { icon: Shield, text: "Protection" },
+    { icon: Bolt, text: "Settings" }
+  ]
+  const majorVersion = "0"
+  const minorVersion = "1"
+  const patchVersion = "0"
+
   return (
-    <div className="w-screen h-screen bg-transparent p-4 overflow-hidden">
+    <div className="w-screen h-screen bg-transparent p-2 overflow-hidden">
+      <div className="w-full h-full bg-white rounded-xl shadow-lg">
+        <ul className="menu menu-md rounded-l-xl bg-base-200 w-40 h-full">
+          <div className="flex items-center justify-center my-8">
+            <Shield size={36} />
+          </div>
+          {menuItems.map((item) => (
+            <li key={item.text} className="my-2">
+              <a className="flex items-center">
+                <item.icon />
+                {item.text}
+              </a>
+            </li>
+          ))}
+          <span className="mt-auto self-center mb-4">{majorVersion}.{minorVersion}.{patchVersion}</span>
+        </ul>
+      </div>
     </div>
   );
 }
