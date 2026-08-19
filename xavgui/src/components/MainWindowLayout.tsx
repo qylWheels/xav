5ac09a6b94c66d0f8874de33f92b24b75378a390
qylWheels <command1748165360@126.com>
2026-08-20 00:57:16 +0800

    Add MainWindowLayout component with sidebar and title bar.

---
+++ xavgui/src/components/MainWindowLayout.tsx
@@ -0,0 +1,21 @@
+import MainSidebar from "@/components/MainSidebar";
+import TitleBar from "@/components/TitleBar";
+import { Outlet } from "react-router-dom";
+
+function MainWindowLayout() {
+    return (
+        <div className="w-screen h-screen bg-transparent p-2 overflow-hidden">
+            <div className="w-full h-full bg-white rounded-xl shadow-lg flex">
+                <MainSidebar />
+                <div className="w-full h-full flex flex-col">
+                    <TitleBar />
+                    <div className="w-full h-auto">
+                        <Outlet />
+                    </div>
+                </div>
+            </div >
+        </div >
+    )
+}
+
+export default MainWindowLayout;
