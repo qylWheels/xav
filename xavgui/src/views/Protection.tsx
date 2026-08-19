f7e44fbf1688a5fbea879c3c9c19ae1b41d4be02
qylWheels <command1748165360@126.com>
2026-08-19 23:01:54 +0800

    Refactor Protection component to include process statistics and enhance UI layout.

--- xavgui/src/views/Protection.tsx
+++ xavgui/src/views/Protection.tsx
@@ -1,3 +1,5 @@
+import { AppWindow, SquareChartGantt } from "lucide-react";
+
 function Protection() {
     return (
         <div className="w-full flex flex-col">
@@ -5,9 +7,23 @@ function Protection() {
                 <div className="card-body">
                     <h2 className="card-title">Proactive Protection</h2>
                     <p>Monitor and analyze process behaviors to detect and prevent potential/unknown threats</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
+                    <div className="flex items-center">
+                        <div className="stat flex-2">
+                            <div className="stat-title">Active Process</div>
+                            <div className="stat-value">813</div>
+                            <div className="stat-figure">
+                                <AppWindow />
+                            </div>
+                        </div>
+                        <div className="stat flex-3">
+                            <div className="stat-title">Total Events</div>
+                            <div className="stat-value">123,972</div>
+                            <div className="stat-figure">
+                                <SquareChartGantt />
+                            </div>
+                        </div>
                     </div>
+                    <button className="btn mt-2">View Process Tree</button>
                 </div>
             </div>
             <div className="card border mx-4 mt-4">
