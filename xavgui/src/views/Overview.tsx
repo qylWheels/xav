44ca80073d8acc44002e6b61476ec9eb82cc8ac8
qylWheels <command1748165360@126.com>
2026-08-19 14:16:33 +0800

    Add overview section with system safety message in Overview.tsx.

--- xavgui/src/views/Overview.tsx
+++ xavgui/src/views/Overview.tsx
@@ -1,6 +1,17 @@
+import { ShieldCheck } from "lucide-react";
+
 function Overview() {
     return (
-        <div></div>
+        <div className="flex-col">
+            <div className="w-full h-16"></div>
+            <div className="flex">
+                <ShieldCheck size={64} color="var(--color-success)" className="ml-16 mr-8" />
+                <div className="flex-col">
+                    <div className="text-2xl font-bold">Your System is Safe</div>
+                    <div className="mt-2">XAV is Protecting Your System</div>
+                </div>
+            </div>
+        </div>
     );
 }
 
