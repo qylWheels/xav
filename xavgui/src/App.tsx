4fcab5fcb13abd898f0fcffa7d79ed4147c12a47
qylWheels <command1748165360@126.com>
2026-08-19 12:42:57 +0800

    Implement titlebar in App.tsx.

--- xavgui/src/App.tsx
+++ xavgui/src/App.tsx
@@ -2,7 +2,7 @@ import "@/index.css"
 import "@/App.css"
 import Overview from "@/views/Overview";
 import Scan from "@/views/Scan";
-import { Shield, Search, LayoutDashboard, Bolt } from "lucide-react";
+import { Shield, Search, LayoutDashboard, Bolt, Minus, X } from "lucide-react";
 
 function App() {
   const menuItems = [
@@ -17,10 +17,10 @@ function App() {
 
   return (
     <div className="w-screen h-screen bg-transparent p-2 overflow-hidden">
-      <div className="w-full h-full bg-white rounded-xl shadow-lg">
-        <ul className="menu menu-md rounded-l-xl bg-base-200 w-40 h-full">
-          <div className="flex items-center justify-center my-8">
-            <Shield size={36} />
+      <div className="w-full h-full bg-white rounded-xl shadow-lg flex">
+        <ul className="menu menu-md rounded-l-xl bg-base-200 w-48 h-full">
+          <div data-tauri-drag-region className="flex items-center justify-center py-8">
+            <Shield data-tauri-drag-region size={36} className="pointer-events-none" />
           </div>
           {menuItems.map((item) => (
             <li key={item.text} className="my-2">
@@ -32,6 +32,10 @@ function App() {
           ))}
           <span className="mt-auto self-center mb-4">{majorVersion}.{minorVersion}.{patchVersion}</span>
         </ul>
+        <div data-tauri-drag-region className="w-full h-1/9 flex justify-end items-center p-4">
+          <button className="btn btn-ghost btn-sm"><Minus size={16} /></button>
+          <button className="btn btn-ghost btn-sm"><X size={16} /></button>
+        </div>
       </div>
     </div>
   );
