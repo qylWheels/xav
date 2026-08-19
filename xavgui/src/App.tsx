66cbd49cd6193846e80353c5f015f929c2124665
qylWheels <command1748165360@126.com>
2026-08-20 01:04:09 +0800

    Refactor App component to use MainWindowLayout and streamline routing structure.

--- xavgui/src/App.tsx
+++ xavgui/src/App.tsx
@@ -3,33 +3,25 @@ import Overview from "@/views/Overview";
 import Scan from "@/views/Scan";
 import Protection from "@/views/Protection";
 import ProcessTree from "@/views/ProcessTree";
-import { Shield, Search, LayoutDashboard, Bolt, Minus, X } from "lucide-react";
-import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
-import { useState } from "react";
-import MainSidebar from "@/components/MainSidebar";
-import TitleBar from "@/components/TitleBar";
+import { Routes, Route } from 'react-router-dom';
+import MainWindowLayout from "@/components/MainWindowLayout";
+import Settings from "@/views/Settings";
+import StandAloneWindowLayout from "@/components/StandAloneWindowLayout";
 
 function App() {
   return (
-    <div className="w-screen h-screen bg-transparent p-2 overflow-hidden">
-      <div className="w-full h-full bg-white rounded-xl shadow-lg flex">
-        <BrowserRouter>
-          <MainSidebar />
-          <div className="w-full h-full flex-col">
-            <TitleBar />
-            <div className="w-full h-auto">
-              <Routes>
-                <Route path="/" element={<Overview />} />
-                <Route path="/overview" element={<Overview />} />
-                <Route path="/scan" element={<Scan />} />
-                <Route path="/protection" element={<Protection />} />
-                <Route path="/process-tree" element={<ProcessTree />} />
-              </Routes>
-            </div>
-          </div>
-        </BrowserRouter>
-      </div >
-    </div >
+    <Routes>
+      <Route path="/" element={<MainWindowLayout />}>
+        <Route index element={<Overview />} />
+        <Route path="/overview" element={<Overview />} />
+        <Route path="/scan" element={<Scan />} />
+        <Route path="/protection" element={<Protection />} />
+        <Route path="/settings" element={<Settings />} />
+      </Route>
+      <Route path="/process-tree" element={<StandAloneWindowLayout />}>
+        <Route index element={<ProcessTree />} />
+      </Route>
+    </Routes>
   );
 }
 
