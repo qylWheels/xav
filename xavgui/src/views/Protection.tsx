ad1f78c1720e1e139d55e1393816af6fbe0453f1
qylWheels <command1748165360@126.com>
2026-08-20 23:58:34 +0800

    Refactor layout of Protection component for improved structure and spacing.

--- xavgui/src/views/Protection.tsx
+++ xavgui/src/views/Protection.tsx
@@ -16,8 +16,9 @@ function Protection() {
     // };
 
     return (
-        <div className="w-full flex flex-col mb-8">
-            <div className="card border mx-4">
+        <div className="flex flex-col mb-8 mx-6">
+            <h1 className="text-2xl font-bold">Protection</h1>
+            <div className="card border mt-4">
                 <div className="card-body">
                     <h2 className="card-title">Proactive Protection</h2>
                     <p>Monitor and analyze process behaviors to detect and prevent potential/unknown threats</p>
@@ -43,7 +44,7 @@ function Protection() {
                     </div>
                 </div>
             </div>
-            <div className="card border mx-4 mt-4">
+            <div className="card border mt-6">
                 <div className="card-body">
                     <h2 className="card-title">HIPS</h2>
                     <p>HIPS (Host Intrusion Prevention System) blocks malicious behaviors to mitigate the risk of malware attacks</p>
