01767a3586a54375ae88f1d9a166e20204f16ce3
qylWheels <command1748165360@126.com>
2026-08-20 14:48:58 +0800

    Set table height of ProcessViewer.

--- xavgui/src/views/ProcessViewer.tsx
+++ xavgui/src/views/ProcessViewer.tsx
@@ -38,36 +38,38 @@ function ProcessViewer() {
             <div className="card card-border bg-base-100">
                 <div className="card-body">
                     <h2 className="card-title">Active Processes</h2>
-                    <table className="table table-pin-rows mt-2">
-                        <thead>
-                            <tr>
-                                <th>PID</th>
-                                <td>Path</td>
-                                <td>Security Level</td>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {processList.map((item, _index) => (
-                                <tr className="hover:bg-base-300">
-                                    <th>{item.pid}</th>
-                                    <td>{item.path}</td>
-                                    <td>
-                                        <div>
-                                            <div className="inline-grid *:[grid-area:1/1]  ">
-                                                <div className={`status ${securityLevelToStatus(item.securityLevel)} ${item.securityLevel === "Low" ? "animate-ping" : ""}`}></div>
-                                                <div className={`status ${securityLevelToStatus(item.securityLevel)}`}></div>
-                                            </div>
-                                            <span className="ml-4">{item.securityLevel}</span>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <button><Ellipsis /></button>
-                                    </td>
+                    <div className="overflow-y-auto h-115">
+                        <table className="table table-pin-rows mt-2">
+                            <thead>
+                                <tr>
+                                    <td>PID</td>
+                                    <td>Path</td>
+                                    <td>Security Level</td>
+                                    <td>Actions</td>
                                 </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                {processList.map((item, _index) => (
+                                    <tr className="hover:bg-base-300">
+                                        <th>{item.pid}</th>
+                                        <td>{item.path}</td>
+                                        <td>
+                                            <div>
+                                                <div className="inline-grid *:[grid-area:1/1]  ">
+                                                    <div className={`status ${securityLevelToStatus(item.securityLevel)} ${item.securityLevel === "Low" ? "animate-ping" : ""}`}></div>
+                                                    <div className={`status ${securityLevelToStatus(item.securityLevel)}`}></div>
+                                                </div>
+                                                <span className="ml-4">{item.securityLevel}</span>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <button><Ellipsis /></button>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
 
