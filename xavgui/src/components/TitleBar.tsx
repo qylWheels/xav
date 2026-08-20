d131bc3d750d9d3e19b6bf54854ec547fea79be3
qylWheels <command1748165360@126.com>
2026-08-20 13:44:31 +0800

    Implement close functionality in TitleBar component.

--- xavgui/src/components/TitleBar.tsx
+++ xavgui/src/components/TitleBar.tsx
@@ -1,10 +1,16 @@
+import { getCurrentWebviewWindow } from "@tauri-apps/api/webviewWindow";
 import { Minus, X } from "lucide-react";
 
 function TitleBar() {
+    const handleClose = async () => {
+        const currentWindow = getCurrentWebviewWindow();
+        await currentWindow.close();
+    };
+
     return (
         <div data-tauri-drag-region className="w-full h-1/9 flex justify-end items-center p-4">
             <button className="btn btn-ghost btn-sm"><Minus size={16} /></button>
-            <button className="btn btn-ghost btn-sm"><X size={16} /></button>
+            <button className="btn btn-ghost btn-sm" onClick={handleClose}><X size={16} /></button>
         </div>
     );
 }
