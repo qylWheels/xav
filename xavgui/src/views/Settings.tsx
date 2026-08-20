4894e656bb12cd8b2562c0470460510cb3cfc92e
qylWheels <command1748165360@126.com>
2026-08-21 00:29:12 +0800

    Add proactive protection strategies to Settings component.

--- xavgui/src/views/Settings.tsx
+++ xavgui/src/views/Settings.tsx
@@ -1,6 +1,35 @@
 function Settings() {
+    const proactiveProtectionStrategies = [
+        "Rule Based Strategy", "Anomaly-Based Detection Strategy"
+    ]
+
     return (
-        <div></div>
+        <div className="flex flex-col mx-6">
+            <h1 className="text-2xl font-bold">Settings</h1>
+            <div className="card border mt-4">
+                <div className="card-body">
+                    <h2 className="card-title">Protection</h2>
+                    <div className="divider divider-start font-bold">Proactive Protection</div>
+                    <ul className="list">
+                        <div className="collapse collapse-arrow bg-base-100 border border-base-300">
+                            <input type="checkbox" />
+                            <div className="collapse-title">Proactive Protection Strategy</div>
+                            <div className="collapse-content">
+                                {proactiveProtectionStrategies.map((strategy, _index) => (
+                                    <fieldset className="fieldset">
+                                        <label className="label">
+                                            <input type="checkbox" defaultChecked className="checkbox" />
+                                            {strategy}
+                                        </label>
+                                    </fieldset>
+                                ))}
+                            </div>
+                        </div>
+                    </ul>
+                    <div className="divider divider-start font-bold">HIPS</div>
+                </div>
+            </div>
+        </div>
     )
 }
 
