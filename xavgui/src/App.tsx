1ef274e849e05a3571a5909b3e8ac3a78398b7a3
qylWheels <command1748165360@126.com>
2026-08-21 20:29:31 +0800

    Implement WebSocket connection and message handling in App component.

--- xavgui/src/App.tsx
+++ xavgui/src/App.tsx
@@ -7,8 +7,28 @@ import { Routes, Route } from 'react-router-dom';
 import MainWindowLayout from "@/components/MainWindowLayout";
 import Settings from "@/views/Settings";
 import StandAloneWindowLayout from "@/components/StandAloneWindowLayout";
+import WebSocket from '@tauri-apps/plugin-websocket';
+import { WebviewWindow } from '@tauri-apps/api/webviewWindow';
 
 function App() {
+  const wsconnection = async () => {
+    const ws = await WebSocket.connect('ws://0.0.0.0:8000');
+    const removeListener = ws.addListener((msg) => {
+      console.log('Received Message:', msg);
+      new WebviewWindow('process-viewer', {
+        url: '/#/process-viewer',
+        width: 800,
+        height: 600,
+        decorations: false,
+        transparent: true,
+        shadow: false,
+        maximizable: false,
+        resizable: false
+      });
+    });
+  }
+  wsconnection();
+
   return (
     <Routes>
       <Route path="/" element={<MainWindowLayout />}>
