d930f744cd08ea6d87767ba826e6158264d2d3a3
qylWheels <command1748165360@126.com>
2026-08-21 22:38:00 +0800

    Add OnAccessScanningAlert component for threat notification.

---
+++ xavgui/src/views/OnAccessScanningAlert.tsx
@@ -0,0 +1,32 @@
+import { ShieldAlert } from "lucide-react";
+
+function OnAccessScanningAlert() {
+    return (
+        <div className="flex flex-col">
+            <div className="flex mt-4">
+                <ShieldAlert color="var(--color-error)" size={48} className="ml-8" />
+                <div className="flex flex-col ml-4">
+                    <div className="text-2xl font-bold">Threat Blocked</div>
+                    <div>Xav has just blocked a threat</div>
+                    <div className="mt-4 flex items-center">
+                        <div>Location:</div>
+                        <button className="btn btn-link text-neutral font-normal p-0 ml-2">/usr/bin/bash</button>
+                    </div>
+                    <div className="flex items-center">
+                        <div>Threat:</div>
+                        <div className="ml-2 text-error">Generic.a</div>
+                    </div>
+                </div>
+            </div>
+            <div className="mt-8">
+                <div className="flex justify-end gap-2 mr-4">
+                    <button className="btn btn-link text-neutral">Add to Exclusion List</button>
+                    <button className="btn">Ignore</button>
+                    <button className="btn btn-success justify-self-end">Quarantine</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default OnAccessScanningAlert;
