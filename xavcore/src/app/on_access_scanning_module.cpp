461f19db81b3cd680605c5516a6ad60acc9ee75c
qylWheels <command1748165360@126.com>
2026-08-21 21:04:10 +0800

    Refactor startup function to accept logger parameter and implement on-access scanning initialization with error handling.

--- xavcore/src/app/on_access_scanning_module.cpp
+++ xavcore/src/app/on_access_scanning_module.cpp
@@ -9,13 +9,18 @@
 #include <iostream>
 #include <utility>
 
+#include "xavcore/protection/on_access_scanning/on_access_scanner.h"
+#include "xavcore/scan/exact_hash.h"
+#include "xavcore/scan/normal_scan_strategy.h"
+#include "xavcore/scan/yara_static_heuristic_engine.h"
+
 namespace beast = boost::beast;
 namespace http = beast::http;
 namespace websocket = beast::websocket;
 namespace net = boost::asio;
 using tcp = boost::asio::ip::tcp;
 
-void startup() {
+void startup(spdlog::logger& logger) {
     // I/O context.
     net::io_context ioc{1};
 
@@ -31,6 +36,19 @@ void startup() {
             res.set(http::field::server, "Xavcore Proactive Protection Module");
         }));
     ws.accept();
+
+    // Start on-access scanning.
+    xavcore::ExactHashEngine exact_hash_engine;
+    xavcore::YaraStaticHeuristicEngine yara_static_heuristic_engine;
+    std::unique_ptr<xavcore::IScanStrategy> normal_scan_strategy =
+        std::make_unique<xavcore::NormalScanStrategy>(
+            exact_hash_engine, yara_static_heuristic_engine);
+    xavcore::OnAccessScanner on_access_scanner(logger, *normal_scan_strategy);
+    if (on_access_scanner.start_monitoring().has_error()) {
+        logger.error("Failed to start on-access scanning: {}",
+                     on_access_scanner.start_monitoring().error().message());
+        return;
+    }
 
     // Handle messages from the client
     while (true) {
@@ -49,7 +67,7 @@ int main(int argc, char* argv[]) {
     auto logger = spdlog::stdout_color_mt("Xavcore On-Access Scanning Module");
     logger->set_level(spdlog::level::info);
     try {
-        startup();
+        startup(*logger);
     } catch (std::exception& e) {
         logger->error("Fatal error: {}", e.what());
         cpptrace::generate_trace().print();
