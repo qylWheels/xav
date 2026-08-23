51ef33ebb5127828d5c697656ee45f992ac5ab27
qylWheels <command1748165360@126.com>
2026-08-23 14:37:13 +0800

    Add on-access scanning module with UnixSeqpacket socket connection.

--- xavgui/src-tauri/src/lib.rs
+++ xavgui/src-tauri/src/lib.rs
@@ -10,6 +10,31 @@ pub fn run() {
         .plugin(tauri_plugin_websocket::init())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            use tokio_seqpacket::UnixSeqpacket;
+            use tauri::async_runtime;
+            use tauri::Emitter;
+
+            let app_handle = app.handle().clone();
+            println!("ready to start async task");
+            async_runtime::spawn(async move {
+                let mut socket = UnixSeqpacket::connect("/tmp/xavcore_on_access_scanning_module_socket").await;
+                if let Err(e) = socket {
+                    println!("error while connecting to socket: {}", e);
+                    return Ok(());
+                }
+                let socket = socket.unwrap();
+                let mut buffer = vec![];
+                loop {
+                    let msg = socket.recv(&mut buffer).await?;
+                    let len = msg.bytes_read();
+                    println!("{}", String::from_utf8_lossy(&buffer[..len]));
+                }
+                Ok::<(), std::io::Error>(())
+            });
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
