f33f6cc0ad9495f263383c7542ca602287fc54a0
qylWheels <command1748165360@126.com>
2026-08-23 21:52:34 +0800

    Refactor socket connection handling and improve error logging for event emission.

--- xavgui/src-tauri/src/lib.rs
+++ xavgui/src-tauri/src/lib.rs
@@ -18,7 +18,7 @@ pub fn run() {
             let app_handle = app.handle().clone();
             println!("ready to start async task");
             async_runtime::spawn(async move {
-                let mut socket =
+                let socket =
                     UnixSeqpacket::connect("\0xavcore_on_access_scanning_module_socket").await;
                 if let Err(e) = socket {
                     println!("error while connecting to socket: {}", e);
@@ -31,7 +31,10 @@ pub fn run() {
                     let len = msg.bytes_read();
                     let path = String::from_utf8_lossy(&buffer[..len]);
                     println!("len: {}, content: {}", len, path);
-                    app_handle.emit("threat-detected", path);
+                    let result = app_handle.emit("threat-detected", path);
+                    if result.is_err() {
+                        println!("error while emitting event: {:?}", result);
+                    }
                 }
                 Ok::<(), std::io::Error>(())
             });
