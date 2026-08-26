77c25ff0bb89cb387cd16205ab210e66611a8e9c
qylWheels <command1748165360@126.com>
2026-08-26 22:44:53 +0800

    Refactor Protection component to implement On-Access Scanning settings and remove proactive protection options.

--- xavgui/src/views/settings/Protection.tsx
+++ xavgui/src/views/settings/Protection.tsx
@@ -1,50 +1,36 @@
 function Protection() {
-    const proactiveProtectionStrategies = [
-        "Rule Based Strategy", "Anomaly-Based Detection Strategy"
-    ]
-    const actionWhenDetectedMaliciousActivity = [
-        { action: "Kill Process", status: "" },
-        { action: "Notify Only", status: "selected" },
-    ]
-
     return (
         <div>
             <div className="card border mt-4">
                 <div className="card-body">
-                    <h2 className="card-title">Protection</h2>
-                    <div className="divider divider-start font-bold">Proactive Protection</div>
-                    <ul className="list">
-                        <div className="collapse collapse-arrow bg-base-100 border border-base-300">
-                            <input type="checkbox" />
-                            <div className="collapse-title">Proactive Protection Strategy</div>
-                            <div className="collapse-content">
-                                {proactiveProtectionStrategies.map((strategy, _index) => (
-                                    <fieldset className="fieldset">
-                                        <label className="label">
-                                            <input type="checkbox" defaultChecked className="checkbox" />
-                                            {strategy}
-                                        </label>
-                                    </fieldset>
-                                ))}
-                            </div>
-                        </div>
-                        <div className="collapse collapse-arrow border border-base-300 mt-2">
-                            <input type="checkbox" />
-                            <div className="collapse-title">Action When Detected Malicious Activity</div>
-                            <div className="collapse-content">
-                                <select defaultValue="Choose an action" className="select">
-                                    <option disabled={true}>Choose an action</option>
-                                    {actionWhenDetectedMaliciousActivity.map((action, _index) => (
-                                        <option selected={action.status === "selected"}>{action.action}</option>
-                                    ))}
-                                </select>
-                            </div>
-                        </div>
-                    </ul>
-                    <div className="divider divider-start font-bold">HIPS</div>
+                    <h2 className="card-title">On-Access Scanning</h2>
+
+                    <div className="divider divider-start font-bold">Timing</div>
+                    <div className="flex flex-col">
+                        <label className="label text-base-content">
+                            <input type="radio" name="timing" className="radio" defaultChecked />
+                            Before execution (Low performance impact)
+                        </label>
+                        <label className="label text-base-content mt-4">
+                            <input type="radio" name="timing" className="radio" />
+                            Before execution and read (High performance impact)
+                        </label>
+                    </div>
+
+                    <div className="divider divider-start font-bold">Action When Detected Threat</div>
+                    <div className="flex flex-col">
+                        <label className="label text-base-content">
+                            <input type="radio" name="action" className="radio" defaultChecked />
+                            Ask me
+                        </label>
+                        <label className="label text-base-content mt-4">
+                            <input type="radio" name="action" className="radio" />
+                            Quarantine automatically
+                        </label>
+                    </div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
