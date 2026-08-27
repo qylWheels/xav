0539db46fece1a8ad68dbea39c59f74a9e464bfd
qylWheels <command1748165360@126.com>
2026-08-27 14:04:11 +0800

    Add FileFingerprint struct for file metadata caching.

---
+++ xavcore/src/include/xavcore/types/file_fingerprint.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <sys/stat.h>
+
+#include <chrono>
+#include <cstdint>
+#include <filesystem>
+#include <string>
+
+namespace xavcore {
+namespace types {
+// For cache.
+struct FileFingerprint {
+    std::uint64_t inode;
+    std::uint64_t size;
+    std::chrono::time_point<std::chrono::system_clock> mtime;
+    std::chrono::time_point<std::chrono::system_clock> ctime;
+
+    FileFingerprint(int fd) {
+        struct stat st;
+        if (fstat(fd, &st) != 0) {
+            throw std::runtime_error("fstat failed");
+        }
+
+        inode = st.st_ino;
+        size = st.st_size;
+        mtime = std::chrono::system_clock::time_point(
+            std::chrono::seconds(st.st_mtim.tv_sec) +
+            std::chrono::nanoseconds(st.st_mtim.tv_nsec));
+        ctime = std::chrono::system_clock::time_point(
+            std::chrono::seconds(st.st_ctim.tv_sec) +
+            std::chrono::nanoseconds(st.st_ctim.tv_nsec));
+    }
+
+    FileFingerprint(const char *path)
+        : FileFingerprint(std::filesystem::path(path)) {}
+
+    FileFingerprint(std::string path)
+        : FileFingerprint(std::filesystem::path(path)) {}
+
+    FileFingerprint(std::filesystem::path path) {
+        struct stat st;
+        if (stat(path.c_str(), &st) != 0) {
+            throw std::runtime_error("stat failed");
+        }
+
+        inode = st.st_ino;
+        size = st.st_size;
+        mtime = std::chrono::system_clock::time_point(
+            std::chrono::seconds(st.st_mtim.tv_sec) +
+            std::chrono::nanoseconds(st.st_mtim.tv_nsec));
+        ctime = std::chrono::system_clock::time_point(
+            std::chrono::seconds(st.st_ctim.tv_sec) +
+            std::chrono::nanoseconds(st.st_ctim.tv_nsec));
+    }
+};
+}  // namespace types
+}  // namespace xavcore
