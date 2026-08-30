8cdf9154bcd2a23a306f3aaebe3157f26f41a50a
qylWheels <command1748165360@126.com>
2026-08-30 14:48:35 +0800

    Add ASLRInspectionRule class for detecting ASLR inspection events.

---
+++ xavcore/src/include/xavcore/protection/proactive_protection/event_listener/rule_based_detection_listener/rules/aslr_inspection_rule.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "xavcore/protection/proactive_protection/event_listener/rule_based_detection_listener/rule_interfaces.h"
+
+namespace xavcore {
+namespace rule_based_detection_listener_rules {
+class ASLRInspectionRule : public IRuleBasedDetectionListenerRule {
+public:
+    ASLRInspectionRule();
+    ~ASLRInspectionRule();
+    ASLRInspectionRule(const ASLRInspectionRule&) = delete;
+    ASLRInspectionRule& operator=(const ASLRInspectionRule&) = delete;
+    ASLRInspectionRule(ASLRInspectionRule&&) = delete;
+    ASLRInspectionRule& operator=(ASLRInspectionRule&&) = delete;
+
+public:  // IRuleBasedDetectionListenerRule methods.
+    virtual std::string name() override;
+    virtual std::string description() override;
+    virtual outcome::result<std::uint8_t> apply(
+        std::span<std::reference_wrapper<IEvent>> event_seq) override;
+    virtual std::size_t event_seq_size_hint() override;
+};
+}  // namespace rule_based_detection_listener_rules
+}  // namespace xavcore
---
+++ xavcore/src/protection/proactive_protection/event_listener/rule_based_detection_listener/rules/aslr_inspection_rule.cpp
@@ -0,0 +1,92 @@
+#include "xavcore/protection/proactive_protection/event_listener/rule_based_detection_listener/rules/aslr_inspection_rule.h"
+
+#include <sys/syscall.h>
+
+#include <cstdlib>
+#include <iostream>
+
+#include "xavcore/protection/proactive_protection/event_provider/syscall_event_provider/syscall_event.h"
+
+namespace xavcore {
+namespace rule_based_detection_listener_rules {
+ASLRInspectionRule::ASLRInspectionRule() = default;
+
+ASLRInspectionRule::~ASLRInspectionRule() = default;
+
+std::string ASLRInspectionRule::name() { return "aslr_inspection_rule"; }
+
+std::string ASLRInspectionRule::description() {
+    return "A rule to detect ASLR inspection";
+}
+
+outcome::result<std::uint8_t> ASLRInspectionRule::apply(
+    std::span<std::reference_wrapper<IEvent>> event_seq) {
+    for (const auto event : event_seq) {
+        try {
+            const auto& syscall_event =
+                dynamic_cast<const SyscallEvent&>(event.get());
+
+            bool id_match =
+                (syscall_event.id == SYS_open ||
+                 syscall_event.id == SYS_openat ||
+                 syscall_event.id == SYS_openat2 ||
+                 syscall_event.id == SYS_read || syscall_event.id == SYS_write);
+            if (!id_match) {
+                continue;
+            }
+
+            std::string path;
+            try {
+                switch (syscall_event.id) {
+                    case SYS_open: {
+                        path = std::get<OpenSyscallAdditionalData>(
+                                   syscall_event.additional_data)
+                                   .path.value();
+                        break;
+                    }
+                    case SYS_openat: {
+                        path = std::get<OpenatSyscallAdditionalData>(
+                                   syscall_event.additional_data)
+                                   .path.value();
+                        break;
+                    }
+                    case SYS_openat2: {
+                        path = std::get<Openat2SyscallAdditionalData>(
+                                   syscall_event.additional_data)
+                                   .path.value();
+                        break;
+                    }
+                    case SYS_read: {
+                        path = std::get<ReadSyscallAdditionalData>(
+                                   syscall_event.additional_data)
+                                   .fd_path.value();
+                        break;
+                    }
+                    case SYS_write: {
+                        path = std::get<WriteSyscallAdditionalData>(
+                                   syscall_event.additional_data)
+                                   .fd_path.value();
+                        break;
+                    }
+                    default: {
+                        std::abort();  // Unreachable.
+                    }
+                }
+            } catch (...) {
+                continue;
+            }
+
+            if (path == "/proc/sys/kernel/randomize_va_space") {
+                std::cout << "ASLR inspection detected" << std::endl;
+                return 20;
+            }
+        } catch (...) {
+            continue;
+        }
+    }
+    return 0;
+}
+
+std::size_t ASLRInspectionRule::event_seq_size_hint() { return 1; }
+}  // namespace rule_based_detection_listener_rules
+}  // namespace xavcore
