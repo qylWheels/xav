4b2513920e3594f9fa6bf6e79fca65ac8cbb1ec6
qylWheels <command1748165360@126.com>
2026-09-04 14:34:01 +0800

    Add CgroupReleaseAgentRuleWarningInfo struct and implement push_event, register_warning_callback, and unregister_warning_callback methods.

--- xavcore/src/include/xavcore/protection/proactive_protection/event_listener/rule_based_detection_listener/rules/cgroup_release_agent_rule.h
+++ xavcore/src/include/xavcore/protection/proactive_protection/event_listener/rule_based_detection_listener/rules/cgroup_release_agent_rule.h
@@ -1,9 +1,20 @@
 #pragma once
 
+#include <functional>
+#include <unordered_set>
+
 #include "xavcore/protection/proactive_protection/event_listener/rule_based_detection_listener/rule_interfaces.h"
 
 namespace xavcore {
 namespace rule_based_detection_listener_rules {
+struct CgroupReleaseAgentRuleWarningInfo : public IRuleWarningInfo {
+    CgroupReleaseAgentRuleWarningInfo(const std::string& path) : path(path) {}
+
+    virtual std::uint8_t severity() override { return 70; }
+
+    std::string path;
+};
+
 class CgroupReleaseAgentRule : public IRuleBasedDetectionListenerRule {
 public:
     CgroupReleaseAgentRule();
@@ -19,6 +30,15 @@ public:
     virtual outcome::result<std::uint8_t> apply(
         std::span<std::reference_wrapper<IEvent>> event_seq) override;
     virtual std::size_t event_seq_size_hint() override;
+    virtual outcome::result<void> push_event(IEvent& event) override;
+    virtual outcome::result<void> register_warning_callback(
+        std::function<void(IRuleWarningInfo&)>& cb) override;
+    virtual outcome::result<void> unregister_warning_callback(
+        std::function<void(IRuleWarningInfo&)>& cb) override;
+
+private:
+    std::unordered_set<std::function<void(IRuleWarningInfo&)>*>
+        callbacks_on_warning_;
 };
 }  // namespace rule_based_detection_listener_rules
 }  // namespace xavcore
--- xavcore/src/protection/proactive_protection/event_listener/rule_based_detection_listener/rules/cgroup_release_agent_rule.cpp
+++ xavcore/src/protection/proactive_protection/event_listener/rule_based_detection_listener/rules/cgroup_release_agent_rule.cpp
@@ -4,6 +4,7 @@
 
 #include <cstdlib>
 #include <regex>
+#include <system_error>
 
 #include "xavcore/protection/proactive_protection/event_provider/syscall_event_provider/syscall_event.h"
 
@@ -23,51 +24,75 @@ std::string CgroupReleaseAgentRule::description() {
 
 outcome::result<std::uint8_t> CgroupReleaseAgentRule::apply(
     std::span<std::reference_wrapper<IEvent>> event_seq) {
-    for (const auto event : event_seq) {
-        try {
-            const auto& syscall_event =
-                dynamic_cast<const SyscallEvent&>(event.get());
-
-            bool id_match = (syscall_event.id == SYS_write ||
-                             syscall_event.id == SYS_rename ||
-                             syscall_event.id == SYS_renameat ||
-                             syscall_event.id == SYS_renameat2);
-            if (!id_match) {
-                continue;
-            }
-
-            std::string path1, path2;
-            switch (syscall_event.id) {
-                case SYS_write:
-                    path1 = std::get<WriteSyscallAdditionalData>(
-                                syscall_event.additional_data)
-                                .fd_path.value();
-                    break;
-                case SYS_rename:
-                case SYS_renameat:
-                case SYS_renameat2:
-                    path1 = std::get<RenameSyscallAdditionalData>(
-                                syscall_event.additional_data)
-                                .old_path.value();
-                    path2 = std::get<RenameSyscallAdditionalData>(
-                                syscall_event.additional_data)
-                                .new_path.value();
-                    break;
-                default:
-                    std::abort();  // Unreachable.
-            }
-
-            std::regex r{".*release_agent"};
-            if (std::regex_match(path1, r) || std::regex_match(path2, r)) {
-                return 70;
-            }
-        } catch (...) {
-            continue;
-        }
-    }
-    return 0;
+    return std::errc::not_supported;
 }
 
 std::size_t CgroupReleaseAgentRule::event_seq_size_hint() { return 1; }
+
+outcome::result<void> CgroupReleaseAgentRule::push_event(IEvent& event) {
+    try {
+        const auto& syscall_event = dynamic_cast<const SyscallEvent&>(event);
+
+        bool id_match =
+            (syscall_event.id == SYS_write || syscall_event.id == SYS_rename ||
+             syscall_event.id == SYS_renameat ||
+             syscall_event.id == SYS_renameat2);
+        if (!id_match) {
+            // Ignore.
+            return outcome::success();
+        }
+
+        std::string path1, path2;
+        switch (syscall_event.id) {
+            case SYS_write:
+                path1 = std::get<WriteSyscallAdditionalData>(
+                            syscall_event.additional_data)
+                            .fd_path.value();
+                break;
+            case SYS_rename:
+            case SYS_renameat:
+            case SYS_renameat2:
+                path1 = std::get<RenameSyscallAdditionalData>(
+                            syscall_event.additional_data)
+                            .old_path.value();
+                path2 = std::get<RenameSyscallAdditionalData>(
+                            syscall_event.additional_data)
+                            .new_path.value();
+                break;
+            default:
+                std::abort();  // Unreachable.
+        }
+
+        std::regex r{".*release_agent"};
+        if (std::regex_match(path1, r)) {
+            auto info = CgroupReleaseAgentRuleWarningInfo(path1);
+            for (auto cb : this->callbacks_on_warning_) {
+                (*cb)(info);
+            }
+        } else if (std::regex_match(path2, r)) {
+            auto info = CgroupReleaseAgentRuleWarningInfo(path2);
+            for (auto cb : this->callbacks_on_warning_) {
+                (*cb)(info);
+            }
+        } else {
+            // Ignore.
+        }
+    } catch (...) {
+        // Ignore.
+    }
+    return outcome::success();
+}
+
+outcome::result<void> CgroupReleaseAgentRule::register_warning_callback(
+    std::function<void(IRuleWarningInfo&)>& cb) {
+    this->callbacks_on_warning_.insert(&cb);
+    return outcome::success();
+}
+
+outcome::result<void> CgroupReleaseAgentRule::unregister_warning_callback(
+    std::function<void(IRuleWarningInfo&)>& cb) {
+    this->callbacks_on_warning_.erase(&cb);
+    return outcome::success();
+}
 }  // namespace rule_based_detection_listener_rules
 }  // namespace xavcore
