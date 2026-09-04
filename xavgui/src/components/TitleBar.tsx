b53dc0068091e8dedac6fbed2a4bcf31843aad4f
qylWheels <command1748165360@126.com>
2026-09-04 23:42:41 +0800

    Add title prop to TitleBar component for dynamic title display.

--- xavgui/src/components/TitleBar.tsx
+++ xavgui/src/components/TitleBar.tsx
@@ -1,16 +1,19 @@
 import { getCurrentWebviewWindow } from "@tauri-apps/api/webviewWindow";
 import { Minus, X } from "lucide-react";
 
-function TitleBar() {
+function TitleBar(props: { title: string }) {
     const handleClose = async () => {
         const currentWindow = getCurrentWebviewWindow();
         await currentWindow.close();
     };
 
     return (
-        <div data-tauri-drag-region className="w-full h-1/9 flex justify-end items-center p-4">
-            <button className="btn btn-ghost btn-sm"><Minus size={16} /></button>
-            <button className="btn btn-ghost btn-sm" onClick={handleClose}><X size={16} /></button>
+        <div data-tauri-drag-region className="w-full h-1/9 flex items-center">
+            <div data-tauri-drag-region className="font-bold m-auto">{props.title}</div>
+            <div data-tauri-drag-region className="flex justify-end items-center p-4">
+                <button className="btn btn-ghost btn-sm"><Minus size={16} /></button>
+                <button className="btn btn-ghost btn-sm" onClick={handleClose}><X size={16} /></button>
+            </div>
         </div>
     );
 }
