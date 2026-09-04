d300be0fc6704142cca3068d68a9968bed0ba353
qylWheels <command1748165360@126.com>
2026-09-04 23:43:14 +0800

    Add title prop to StandAloneWindowLayout for TitleBar component.

--- xavgui/src/components/StandAloneWindowLayout.tsx
+++ xavgui/src/components/StandAloneWindowLayout.tsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom";
 import TitleBar from "./TitleBar";
 
-function StandAloneWindowLayout() {
+function StandAloneWindowLayout(props: { title: string }) {
     return (
         <div className="w-screen h-screen bg-transparent p-2 overflow-hidden">
             <div className="w-full h-full bg-white rounded-xl shadow-lg flex">
                 <div className="w-full h-full flex flex-col">
-                    <TitleBar />
+                    <TitleBar title={props.title} />
                     <div className="w-full h-auto">
                         <Outlet />
                     </div>
