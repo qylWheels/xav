98c69f4d271ece421ee6d0e2d1b6f813e21bf0a0
qylWheels <command1748165360@126.com>
2026-09-04 20:10:22 +0800

    Add DefaultLoaderModifyRuleWarningInfo and implement push_event, register_warning_callback, and unregister_warning_callback methods in DefaultLoaderModifyRule.

--- xavcore/src/include/xavcore/protection/proactive_protection/event_listener/rule_based_detection_listener/rules/default_loader_modify_rule.h
+++ xavcore/src/include/xavcore/protection/proactive_protection/event_listener/rule_based_detection_listener/rules/default_loader_modify_rule.h
@@ -1,9 +1,22 @@
 #pragma once
 
+#include <cstdint>
+#include <functional>
+#include <string>
+#include <unordered_set>
+
 #include "xavcore/protection/proactive_protection/event_listener/rule_based_detection_listener/rule_interfaces.h"
 
 namespace xavcore {
 namespace rule_based_detection_listener_rules {
+struct DefaultLoaderModifyRuleWarningInfo : public IRuleWarningInfo {
+    DefaultLoaderModifyRuleWarningInfo(std::string path) : path(path) {}
+
+    virtual std::uint8_t severity() override { return 70; }
+
+    std::string path;
+};
+
 class DefaultLoaderModifyRule : public IRuleBasedDetectionListenerRule {
 public:
     DefaultLoaderModifyRule();
@@ -19,6 +32,14 @@ public:  // IRuleBasedDetectionListenerRule methods.
     virtual outcome::result<std::uint8_t> apply(
         std::span<std::reference_wrapper<IEvent>> event_seq) override;
     virtual std::size_t event_seq_size_hint() override;
+    virtual outcome::result<void> push_event(IEvent& event) override;
+    virtual outcome::result<void> register_warning_callback(
+        std::function<void(IRuleWarningInfo&)>& cb) override;
+    virtual outcome::result<void> unregister_warning_callback(
+        std::function<void(IRuleWarningInfo&)>& cb) override;
+
+private:
+    std::unordered_set<std::function<void(IRuleWarningInfo&)>*> cbs_;
 };
 }  // namespace rule_based_detection_listener_rules
 }  // namespace xavcore
--- xavcore/src/protection/proactive_protection/event_listener/rule_based_detection_listener/rules/default_loader_modify_rule.cpp
+++ xavcore/src/protection/proactive_protection/event_listener/rule_based_detection_listener/rules/default_loader_modify_rule.cpp
@@ -4,6 +4,7 @@
 
 #include <cstdlib>
 #include <regex>
+#include <system_error>
 
 #include "xavcore/protection/proactive_protection/event_provider/syscall_event_provider/syscall_event.h"
 
@@ -23,51 +24,74 @@ std::string DefaultLoaderModifyRule::description() {
 
 outcome::result<std::uint8_t> DefaultLoaderModifyRule::apply(
     std::span<std::reference_wrapper<IEvent>> event_seq) {
-    for (const auto event : event_seq) {
-        try {
-            const auto& syscall_event =
-                dynamic_cast<const SyscallEvent&>(event.get());
-
-            bool id_match = (syscall_event.id == SYS_write ||
-                             syscall_event.id == SYS_rename ||
-                             syscall_event.id == SYS_renameat ||
-                             syscall_event.id == SYS_renameat2);
-            if (!id_match) {
-                continue;
-            }
-
-            std::string path1, path2;
-            switch (syscall_event.id) {
-                case SYS_write:
-                    path1 = std::get<WriteSyscallAdditionalData>(
-                                syscall_event.additional_data)
-                                .fd_path.value();
-                    break;
-                case SYS_rename:
-                case SYS_renameat:
-                case SYS_renameat2:
-                    path1 = std::get<RenameSyscallAdditionalData>(
-                                syscall_event.additional_data)
-                                .old_path.value();
-                    path2 = std::get<RenameSyscallAdditionalData>(
-                                syscall_event.additional_data)
-                                .new_path.value();
-                    break;
-                default:
-                    std::abort();  // Unreachable.
-            }
-
-            std::regex r{".*lib.*ld-.*\\.so.*"};
-            if (std::regex_match(path1, r) || std::regex_match(path2, r)) {
-                return 70;
-            }
-        } catch (...) {
-            continue;
-        }
-    }
-    return 0;
+    return std::errc::not_supported;
 }
 
 std::size_t DefaultLoaderModifyRule::event_seq_size_hint() { return 1; }
+
+outcome::result<void> DefaultLoaderModifyRule::push_event(IEvent& event) {
+    try {
+        const auto& syscall_event = dynamic_cast<const SyscallEvent&>(event);
+
+        bool id_match =
+            (syscall_event.id == SYS_write || syscall_event.id == SYS_rename ||
+             syscall_event.id == SYS_renameat ||
+             syscall_event.id == SYS_renameat2);
+        if (!id_match) {
+            return outcome::success();
+        }
+
+        std::string path1, path2;
+        switch (syscall_event.id) {
+            case SYS_write:
+                path1 = std::get<WriteSyscallAdditionalData>(
+                            syscall_event.additional_data)
+                            .fd_path.value();
+                break;
+            case SYS_rename:
+            case SYS_renameat:
+            case SYS_renameat2:
+                path1 = std::get<RenameSyscallAdditionalData>(
+                            syscall_event.additional_data)
+                            .old_path.value();
+                path2 = std::get<RenameSyscallAdditionalData>(
+                            syscall_event.additional_data)
+                            .new_path.value();
+                break;
+            default:
+                std::abort();  // Unreachable.
+        }
+
+        std::regex re{".*lib.*ld-.*\\.so.*"};
+        if (std::regex_match(path1, re)) {
+            auto info = DefaultLoaderModifyRuleWarningInfo(path1);
+            for (auto cb : this->cbs_) {
+                (*cb)(info);
+            }
+        } else if (std::regex_match(path2, re)) {
+            auto info = DefaultLoaderModifyRuleWarningInfo(path2);
+            for (auto cb : this->cbs_) {
+                (*cb)(info);
+            }
+        } else {
+            // Void.
+        }
+    } catch (...) {
+        // Ignore.
+    }
+    return outcome::success();
+}
+
+outcome::result<void> DefaultLoaderModifyRule::register_warning_callback(
+    std::function<void(IRuleWarningInfo&)>& cb) {
+    this->cbs_.insert(&cb);
+    return outcome::success();
+}
+
+outcome::result<void> DefaultLoaderModifyRule::unregister_warning_callback(
+    std::function<void(IRuleWarningInfo&)>& cb) {
+    this->cbs_.erase(&cb);
+    return outcome::success();
+}
 }  // namespace rule_based_detection_listener_rules
 }  // namespace xavcore
