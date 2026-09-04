424a70a40ebc494d93f4c293e117fb72253790bd
qylWheels <command1748165360@126.com>
2026-09-04 23:43:34 +0800

    Add title prop to MainWindowLayout for TitleBar component.

--- xavgui/src/components/MainWindowLayout.tsx
+++ xavgui/src/components/MainWindowLayout.tsx
@@ -2,13 +2,13 @@ import MainSidebar from "@/components/MainSidebar";
 import TitleBar from "@/components/TitleBar";
 import { Outlet } from "react-router-dom";
 
-function MainWindowLayout() {
+function MainWindowLayout(props: { title: string }) {
     return (
         <div className="w-screen h-screen bg-transparent p-2 overflow-hidden">
             <div className="w-full h-full bg-white rounded-xl shadow-lg flex">
                 <MainSidebar />
                 <div className="w-full h-full flex flex-col">
-                    <TitleBar title="" />
+                    <TitleBar title={props.title} />
                     <div className="w-full h-full overflow-y-auto">
                         <Outlet />
                     </div>
