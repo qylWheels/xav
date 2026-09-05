aa1544862c5befbea194ac1cc976f58fd45c053e
qylWheels <command1748165360@126.com>
2026-09-05 21:48:40 +0800

    Add ProcKcoreReadRule warning system with callback registration and event handling.

--- xavcore/src/protection/proactive_protection/event_listener/rule_based_detection_listener/rules/proc_kcore_read_rule.cpp
+++ xavcore/src/protection/proactive_protection/event_listener/rule_based_detection_listener/rules/proc_kcore_read_rule.cpp
@@ -3,6 +3,7 @@
 #include <sys/syscall.h>
 
 #include <regex>
+#include <system_error>
 
 #include "xavcore/protection/proactive_protection/event_provider/syscall_event_provider/syscall_event.h"
 
@@ -20,25 +21,43 @@ std::string ProcKcoreReadRule::description() {
 
 outcome::result<std::uint8_t> ProcKcoreReadRule::apply(
     std::span<std::reference_wrapper<IEvent>> event_seq) {
-    for (const auto event : event_seq) {
-        try {
-            const auto& syscall_event =
-                dynamic_cast<const SyscallEvent&>(event.get());
+    return std::errc::not_supported;
+}
 
+std::size_t ProcKcoreReadRule::event_seq_size_hint() { return 1; }
+
+outcome::result<void> ProcKcoreReadRule::push_event(const IEvent& event) {
+    try {
+        const auto& syscall_event = dynamic_cast<const SyscallEvent&>(event);
+        if (syscall_event.id == SYS_read) {
             std::string path = std::get<ReadSyscallAdditionalData>(
                                    syscall_event.additional_data)
                                    .fd_path.value();
             std::regex re("/proc/kcore");
-            if (syscall_event.id == SYS_read && std::regex_search(path, re)) {
-                return 40;
+            if (std::regex_search(path, re)) {
+                for (auto cb : this->callbacks_on_warning_) {
+                    auto info = ProcKcoreReadRuleWarningInfo(path);
+                    (*cb)(info);
+                }
             }
-        } catch (...) {
-            continue;
         }
+    } catch (...) {
+        // Ignore.
     }
-    return 0;
+    return outcome::success();
 }
 
-std::size_t ProcKcoreReadRule::event_seq_size_hint() { return 1; }
+outcome::result<void> ProcKcoreReadRule::register_warning_callback(
+    std::function<void(const IRuleWarningInfo&)>& cb) {
+    this->callbacks_on_warning_.insert(&cb);
+    return outcome::success();
+}
+
+outcome::result<void> ProcKcoreReadRule::unregister_warning_callback(
+    std::function<void(const IRuleWarningInfo&)>& cb) {
+    this->callbacks_on_warning_.erase(&cb);
+    return outcome::success();
+}
+
 }  // namespace rule_based_detection_listener_rules
 }  // namespace xavcore
